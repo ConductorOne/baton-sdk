315f76b614518b8332a334efe07dc4d067ad78d4
Justin Gallardo <justin.gallardo@conductorone.com>
2022-11-29 15:05:45 -0800

    Add helpers for creating connector objects (#13)

--- pkg/sdk/connector_objects.go
+++ pkg/sdk/connector_objects.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
+	"github.com/conductorone/baton-sdk/pkg/annotations"
 )
 
 func convertIDToString(id interface{}) (string, error) {
@@ -31,7 +32,7 @@ func NewResourceID(resourceType *v2.ResourceType, parentResourceID *v2.ResourceI
 	}
 
 	if parentResourceID != nil {
-		id = fmt.Sprintf("%s:%s", parentResourceID.Resource, id)
+		id = fmt.Sprintf("%s:%s:%s", parentResourceID.ResourceType, parentResourceID.Resource, id)
 	}
 
 	return &v2.ResourceId{
@@ -40,12 +41,166 @@ func NewResourceID(resourceType *v2.ResourceType, parentResourceID *v2.ResourceI
 	}, nil
 }
 
-// NewEntitlementID returns a valid entitlement ID given a resource and permission.
+// NewResource returns a new resource instance with no traits.
+func NewResource(name string, resourceType *v2.ResourceType, parentResourceID *v2.ResourceId, objectID interface{}) (*v2.Resource, error) {
+	rID, err := NewResourceID(resourceType, parentResourceID, objectID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &v2.Resource{
+		Id:               rID,
+		ParentResourceId: parentResourceID,
+		DisplayName:      name,
+	}, nil
+}
+
+// NewUserResource returns a new resource instance with a configured user trait.
+// The trait is configured with the provided email address and profile and status set to enabled.
+func NewUserResource(
+	name string,
+	resourceType *v2.ResourceType,
+	parentResourceID *v2.ResourceId,
+	objectID interface{},
+	primaryEmail string,
+	profile map[string]interface{},
+) (*v2.Resource, error) {
+	ret, err := NewResource(name, resourceType, parentResourceID, objectID)
+	if err != nil {
+		return nil, err
+	}
+
+	userTrait, err := NewUserTrait(primaryEmail, v2.UserTrait_Status_STATUS_ENABLED, profile)
+	if err != nil {
+		return nil, err
+	}
+
+	var annos annotations.Annotations
+	annos.Append(userTrait)
+
+	ret.Annotations = annos
+
+	return ret, nil
+}
+
+// NewGroupResource returns a new resource instance with a configured group trait.
+// The trait is configured with the provided profile.
+func NewGroupResource(
+	name string,
+	resourceType *v2.ResourceType,
+	parentResourceID *v2.ResourceId,
+	objectID interface{},
+	profile map[string]interface{},
+) (*v2.Resource, error) {
+	ret, err := NewResource(name, resourceType, parentResourceID, objectID)
+	if err != nil {
+		return nil, err
+	}
+
+	groupTrait, err := NewGroupTrait(profile)
+	if err != nil {
+		return nil, err
+	}
+
+	var annos annotations.Annotations
+	annos.Append(groupTrait)
+
+	ret.Annotations = annos
+
+	return ret, nil
+}
+
+// NewRoleResource returns a new resource instance with a configured role trait.
+// The trait is configured with the provided profile.
+func NewRoleResource(
+	name string,
+	resourceType *v2.ResourceType,
+	parentResourceID *v2.ResourceId,
+	objectID interface{},
+	profile map[string]interface{},
+) (*v2.Resource, error) {
+	ret, err := NewResource(name, resourceType, parentResourceID, objectID)
+	if err != nil {
+		return nil, err
+	}
+
+	roleTrait, err := NewRoleTrait(profile)
+	if err != nil {
+		return nil, err
+	}
+
+	var annos annotations.Annotations
+	annos.Append(roleTrait)
+
+	ret.Annotations = annos
+
+	return ret, nil
+}
+
+// NewAppResource returns a new resource instance with a configured app trait.
+// The trait is configured with the provided helpURL and profile.
+func NewAppResource(
+	name string,
+	resourceType *v2.ResourceType,
+	parentResourceID *v2.ResourceId,
+	objectID interface{},
+	helpURL string,
+	profile map[string]interface{},
+) (*v2.Resource, error) {
+	ret, err := NewResource(name, resourceType, parentResourceID, objectID)
+	if err != nil {
+		return nil, err
+	}
+
+	appTrait, err := NewAppTrait(helpURL, profile)
+	if err != nil {
+		return nil, err
+	}
+
+	var annos annotations.Annotations
+	annos.Append(appTrait)
+
+	ret.Annotations = annos
+
+	return ret, nil
+}
+
 func NewEntitlementID(resource *v2.Resource, permission string) string {
 	return fmt.Sprintf("%s:%s:%s", resource.Id.ResourceType, resource.Id.Resource, permission)
 }
 
-// NewGrantID returns a valid grant ID given an entitlement and principal resource.
-func NewGrantID(entitlement *v2.Entitlement, principal *v2.Resource) string {
-	return fmt.Sprintf("%s:%s:%s", entitlement.Id, principal.Id.ResourceType, principal.Id.Resource)
+func NewAssignmentEntitlement(resource *v2.Resource, name string, grantableTo ...*v2.ResourceType) *v2.Entitlement {
+	return &v2.Entitlement{
+		Id:          NewEntitlementID(resource, name),
+		DisplayName: name,
+		Slug:        name,
+		GrantableTo: grantableTo,
+		Purpose:     v2.Entitlement_PURPOSE_VALUE_ASSIGNMENT,
+		Resource:    resource,
+	}
+}
+
+func NewPermissionEntitlement(resource *v2.Resource, name string, grantableTo ...*v2.ResourceType) *v2.Entitlement {
+	return &v2.Entitlement{
+		Id:          NewEntitlementID(resource, name),
+		DisplayName: name,
+		Slug:        name,
+		GrantableTo: grantableTo,
+		Purpose:     v2.Entitlement_PURPOSE_VALUE_PERMISSION,
+		Resource:    resource,
+	}
+}
+
+// NewGrant returns a new grant for the given entitlement on the resource for the provided principal resource ID.
+func NewGrant(resource *v2.Resource, entitlementName string, principal *v2.ResourceId) *v2.Grant {
+	entitlement := &v2.Entitlement{
+		Id:       NewEntitlementID(resource, entitlementName),
+		Resource: resource,
+	}
+
+	return &v2.Grant{
+		Entitlement: entitlement,
+		Principal:   &v2.Resource{Id: principal},
+		Id:          fmt.Sprintf("%s:%s:%s", entitlement.Id, principal.ResourceType, principal.Resource),
+	}
 }
--- pkg/sdk/traits.go
+++ pkg/sdk/traits.go
@@ -6,7 +6,7 @@ import (
 )
 
 // NewUserTrait creates a new `UserTrait` with the given primary email, status, asset reference, and profile.
-func NewUserTrait(primaryEmail string, status v2.UserTrait_Status_Status, assetRef *v2.AssetRef, profile map[string]interface{}) (*v2.UserTrait, error) {
+func NewUserTrait(primaryEmail string, status v2.UserTrait_Status_Status, profile map[string]interface{}) (*v2.UserTrait, error) {
 	emails := make([]*v2.UserTrait_Email, 0)
 	if primaryEmail != "" {
 		emails = append(emails, &v2.UserTrait_Email{
@@ -20,33 +20,31 @@ func NewUserTrait(primaryEmail string, status v2.UserTrait_Status_Status, assetR
 		return nil, err
 	}
 
-	ut := &v2.UserTrait{
+	userTrait := &v2.UserTrait{
 		Emails:  emails,
 		Status:  &v2.UserTrait_Status{Status: status},
 		Profile: p,
-		Icon:    assetRef,
 	}
 
-	return ut, nil
+	return userTrait, nil
 }
 
 // NewGroupTrait creates a new `GroupTrait` with the fiven icon, and profile.
-func NewGroupTrait(assetRef *v2.AssetRef, profile map[string]interface{}) (*v2.GroupTrait, error) {
+func NewGroupTrait(profile map[string]interface{}) (*v2.GroupTrait, error) {
 	p, err := structpb.NewStruct(profile)
 	if err != nil {
 		return nil, err
 	}
 
-	ut := &v2.GroupTrait{
+	groupTrait := &v2.GroupTrait{
 		Profile: p,
-		Icon:    assetRef,
 	}
 
-	return ut, nil
+	return groupTrait, nil
 }
 
 // NewAppTrait creates a new `AppTrait` with the given icon, logo, help URL, and profile.
-func NewAppTrait(icon *v2.AssetRef, logo *v2.AssetRef, helpURL string, profile map[string]interface{}) (*v2.AppTrait, error) {
+func NewAppTrait(helpURL string, profile map[string]interface{}) (*v2.AppTrait, error) {
 	p, err := structpb.NewStruct(profile)
 	if err != nil {
 		return nil, err
@@ -54,8 +52,6 @@ func NewAppTrait(icon *v2.AssetRef, logo *v2.AssetRef, helpURL string, profile m
 
 	ut := &v2.AppTrait{
 		Profile: p,
-		Icon:    icon,
-		Logo:    logo,
 		HelpUrl: helpURL,
 	}
 
