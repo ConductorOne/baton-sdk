3d9d2cdeec472b38fed024546377a78c102e4b37
Justin Gallardo <justin.gallardo@conductorone.com>
2023-03-29 10:18:02 -0700

    Add service API and other protobuf lint changes

--- pb/c1/ratelimit/v1/ratelimit.pb.validate.go
+++ pb/c1/ratelimit/v1/ratelimit.pb.validate.go
@@ -336,6 +336,106 @@ var _ interface {
 	ErrorName() string
 } = ReportRequestValidationError{}
 
+// Validate checks the field values on ReportResponse with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *ReportResponse) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on ReportResponse with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in ReportResponseMultiError,
+// or nil if none found.
+func (m *ReportResponse) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *ReportResponse) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if len(errors) > 0 {
+		return ReportResponseMultiError(errors)
+	}
+
+	return nil
+}
+
+// ReportResponseMultiError is an error wrapping multiple validation errors
+// returned by ReportResponse.ValidateAll() if the designated constraints
+// aren't met.
+type ReportResponseMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m ReportResponseMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m ReportResponseMultiError) AllErrors() []error { return m }
+
+// ReportResponseValidationError is the validation error returned by
+// ReportResponse.Validate if the designated constraints aren't met.
+type ReportResponseValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e ReportResponseValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e ReportResponseValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e ReportResponseValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e ReportResponseValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e ReportResponseValidationError) ErrorName() string { return "ReportResponseValidationError" }
+
+// Error satisfies the builtin error interface
+func (e ReportResponseValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sReportResponse.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = ReportResponseValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = ReportResponseValidationError{}
+
 // Validate checks the field values on RateLimitDescriptors with the rules
 // defined in the proto definition for this message. If any rules are
 // violated, the first error encountered is returned, or nil if there are no violations.
