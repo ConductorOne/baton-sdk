467b51bc92a9e195a7ed4a6102678e9a3549128c
Justin Gallardo <justin.gallardo@conductorone.com>
2023-05-11 10:41:18 -0700

    Refactor c1z decoder options, check the environment later on to ensure it always happens. (#61)

--- pkg/dotc1z/file.go
+++ pkg/dotc1z/file.go
@@ -5,15 +5,9 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"strconv"
 	"syscall"
 
 	"github.com/klauspost/compress/zstd"
-)
-
-const (
-	maxDecodedSizeEnvVar    = "BATON_DECODER_MAX_DECODED_SIZE_MB"
-	maxDecoderMemorySizeEnv = "BATON_DECODER_MAX_MEMORY_MB"
 )
 
 func loadC1z(filePath string) (string, error) {
@@ -35,25 +29,7 @@ func loadC1z(filePath string) (string, error) {
 		}
 		defer c1zFile.Close()
 
-		var opts []DecoderOption
-
-		maxDecodedSizeVar := os.Getenv(maxDecodedSizeEnvVar)
-		if maxDecodedSizeVar != "" {
-			maxDecodedSize, err := strconv.ParseUint(maxDecodedSizeVar, 10, 64)
-			if err == nil {
-				opts = append(opts, WithDecoderMaxDecodedSize(maxDecodedSize*1024*1024))
-			}
-		}
-
-		maxDecoderMemorySizeVar := os.Getenv(maxDecoderMemorySizeEnv)
-		if maxDecoderMemorySizeVar != "" {
-			maxDecoderMemorySize, err := strconv.ParseUint(maxDecoderMemorySizeVar, 10, 64)
-			if err == nil {
-				opts = append(opts, WithDecoderMaxMemory(maxDecoderMemorySize*1024*1024))
-			}
-		}
-
-		r, err := NewDecoder(c1zFile, opts...)
+		r, err := NewDecoder(c1zFile)
 		if err != nil {
 			return "", err
 		}
