26e1ab535803d63a4d8ca921caa0fa81b5d4a129
Justin Gallardo <justin.gallardo@conductorone.com>
2023-05-16 11:14:23 -0700

    New protos for working with previous syncs

--- pb/c1/connector/v2/annotation_etag.pb.go
+++ pb/c1/connector/v2/annotation_etag.pb.go
@@ -9,7 +9,7 @@ package v2
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
+	_ "google.golang.org/protobuf/types/known/timestamppb"
 	reflect "reflect"
 	sync "sync"
 )
@@ -68,53 +68,6 @@ func (x *ETag) GetValue() string {
 	return ""
 }
 
-type ETagLastUpdatedAt struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	LastUpdatedAt *timestamppb.Timestamp `protobuf:"bytes,1,opt,name=last_updated_at,json=lastUpdatedAt,proto3" json:"last_updated_at,omitempty"`
-}
-
-func (x *ETagLastUpdatedAt) Reset() {
-	*x = ETagLastUpdatedAt{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_c1_connector_v2_annotation_etag_proto_msgTypes[1]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *ETagLastUpdatedAt) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*ETagLastUpdatedAt) ProtoMessage() {}
-
-func (x *ETagLastUpdatedAt) ProtoReflect() protoreflect.Message {
-	mi := &file_c1_connector_v2_annotation_etag_proto_msgTypes[1]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use ETagLastUpdatedAt.ProtoReflect.Descriptor instead.
-func (*ETagLastUpdatedAt) Descriptor() ([]byte, []int) {
-	return file_c1_connector_v2_annotation_etag_proto_rawDescGZIP(), []int{1}
-}
-
-func (x *ETagLastUpdatedAt) GetLastUpdatedAt() *timestamppb.Timestamp {
-	if x != nil {
-		return x.LastUpdatedAt
-	}
-	return nil
-}
-
 var File_c1_connector_v2_annotation_etag_proto protoreflect.FileDescriptor
 
 var file_c1_connector_v2_annotation_etag_proto_rawDesc = []byte{
@@ -125,16 +78,11 @@ var file_c1_connector_v2_annotation_etag_proto_rawDesc = []byte{
 	0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74,
 	0x61, 0x6d, 0x70, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x1c, 0x0a, 0x04, 0x45, 0x54, 0x61,
 	0x67, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
-	0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x22, 0x57, 0x0a, 0x11, 0x45, 0x54, 0x61, 0x67, 0x4c,
-	0x61, 0x73, 0x74, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x64, 0x41, 0x74, 0x12, 0x42, 0x0a, 0x0f,
-	0x6c, 0x61, 0x73, 0x74, 0x5f, 0x75, 0x70, 0x64, 0x61, 0x74, 0x65, 0x64, 0x5f, 0x61, 0x74, 0x18,
-	0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x1a, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d,
-	0x70, 0x52, 0x0d, 0x6c, 0x61, 0x73, 0x74, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x64, 0x41, 0x74,
-	0x42, 0x36, 0x5a, 0x34, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63,
-	0x6f, 0x6e, 0x64, 0x75, 0x63, 0x74, 0x6f, 0x72, 0x6f, 0x6e, 0x65, 0x2f, 0x62, 0x61, 0x74, 0x6f,
-	0x6e, 0x2d, 0x73, 0x64, 0x6b, 0x2f, 0x70, 0x62, 0x2f, 0x63, 0x31, 0x2f, 0x63, 0x6f, 0x6e, 0x6e,
-	0x65, 0x63, 0x74, 0x6f, 0x72, 0x2f, 0x76, 0x32, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x42, 0x36, 0x5a, 0x34, 0x67, 0x69, 0x74, 0x68, 0x75,
+	0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x6f, 0x6e, 0x64, 0x75, 0x63, 0x74, 0x6f, 0x72, 0x6f,
+	0x6e, 0x65, 0x2f, 0x62, 0x61, 0x74, 0x6f, 0x6e, 0x2d, 0x73, 0x64, 0x6b, 0x2f, 0x70, 0x62, 0x2f,
+	0x63, 0x31, 0x2f, 0x63, 0x6f, 0x6e, 0x6e, 0x65, 0x63, 0x74, 0x6f, 0x72, 0x2f, 0x76, 0x32, 0x62,
+	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -149,19 +97,16 @@ func file_c1_connector_v2_annotation_etag_proto_rawDescGZIP() []byte {
 	return file_c1_connector_v2_annotation_etag_proto_rawDescData
 }
 
-var file_c1_connector_v2_annotation_etag_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_c1_connector_v2_annotation_etag_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
 var file_c1_connector_v2_annotation_etag_proto_goTypes = []interface{}{
-	(*ETag)(nil),                  // 0: c1.connector.v2.ETag
-	(*ETagLastUpdatedAt)(nil),     // 1: c1.connector.v2.ETagLastUpdatedAt
-	(*timestamppb.Timestamp)(nil), // 2: google.protobuf.Timestamp
+	(*ETag)(nil), // 0: c1.connector.v2.ETag
 }
 var file_c1_connector_v2_annotation_etag_proto_depIdxs = []int32{
-	2, // 0: c1.connector.v2.ETagLastUpdatedAt.last_updated_at:type_name -> google.protobuf.Timestamp
-	1, // [1:1] is the sub-list for method output_type
-	1, // [1:1] is the sub-list for method input_type
-	1, // [1:1] is the sub-list for extension type_name
-	1, // [1:1] is the sub-list for extension extendee
-	0, // [0:1] is the sub-list for field type_name
+	0, // [0:0] is the sub-list for method output_type
+	0, // [0:0] is the sub-list for method input_type
+	0, // [0:0] is the sub-list for extension type_name
+	0, // [0:0] is the sub-list for extension extendee
+	0, // [0:0] is the sub-list for field type_name
 }
 
 func init() { file_c1_connector_v2_annotation_etag_proto_init() }
@@ -182,18 +127,6 @@ func file_c1_connector_v2_annotation_etag_proto_init() {
 				return nil
 			}
 		}
-		file_c1_connector_v2_annotation_etag_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*ETagLastUpdatedAt); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -201,7 +134,7 @@ func file_c1_connector_v2_annotation_etag_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_c1_connector_v2_annotation_etag_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
