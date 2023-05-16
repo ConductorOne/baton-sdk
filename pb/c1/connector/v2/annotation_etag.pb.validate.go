c784f5aed7b3ea5402029fcd1d6ac8cb31f96508
Justin Gallardo <justin.gallardo@conductorone.com>
2023-05-16 10:43:25 -0700

    Add new ETagLastUpdatedAt annotation

--- pb/c1/connector/v2/annotation_etag.pb.validate.go
+++ pb/c1/connector/v2/annotation_etag.pb.validate.go
@@ -134,3 +134,134 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = ETagValidationError{}
+
+// Validate checks the field values on ETagLastUpdatedAt with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// first error encountered is returned, or nil if there are no violations.
+func (m *ETagLastUpdatedAt) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on ETagLastUpdatedAt with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// ETagLastUpdatedAtMultiError, or nil if none found.
+func (m *ETagLastUpdatedAt) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *ETagLastUpdatedAt) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if all {
+		switch v := interface{}(m.GetLastUpdatedAt()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ETagLastUpdatedAtValidationError{
+					field:  "LastUpdatedAt",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ETagLastUpdatedAtValidationError{
+					field:  "LastUpdatedAt",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetLastUpdatedAt()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ETagLastUpdatedAtValidationError{
+				field:  "LastUpdatedAt",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if len(errors) > 0 {
+		return ETagLastUpdatedAtMultiError(errors)
+	}
+
+	return nil
+}
+
+// ETagLastUpdatedAtMultiError is an error wrapping multiple validation errors
+// returned by ETagLastUpdatedAt.ValidateAll() if the designated constraints
+// aren't met.
+type ETagLastUpdatedAtMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m ETagLastUpdatedAtMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m ETagLastUpdatedAtMultiError) AllErrors() []error { return m }
+
+// ETagLastUpdatedAtValidationError is the validation error returned by
+// ETagLastUpdatedAt.Validate if the designated constraints aren't met.
+type ETagLastUpdatedAtValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e ETagLastUpdatedAtValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e ETagLastUpdatedAtValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e ETagLastUpdatedAtValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e ETagLastUpdatedAtValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e ETagLastUpdatedAtValidationError) ErrorName() string {
+	return "ETagLastUpdatedAtValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e ETagLastUpdatedAtValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sETagLastUpdatedAt.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = ETagLastUpdatedAtValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = ETagLastUpdatedAtValidationError{}
