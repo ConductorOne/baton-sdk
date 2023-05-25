1786f20970e5e7e775e225a7355b652d2cd58f45
Justin Gallardo <justin.gallardo@conductorone.com>
2023-05-24 18:29:40 -0700

    Add annotations to list grant reader calls

--- pb/c1/reader/v2/grant.pb.validate.go
+++ pb/c1/reader/v2/grant.pb.validate.go
@@ -246,6 +246,40 @@ func (m *GrantsReaderServiceListGrantsForEntitlementRequest) validate(all bool) 
 
 	}
 
+	for idx, item := range m.GetAnnotations() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, GrantsReaderServiceListGrantsForEntitlementRequestValidationError{
+						field:  fmt.Sprintf("Annotations[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, GrantsReaderServiceListGrantsForEntitlementRequestValidationError{
+						field:  fmt.Sprintf("Annotations[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return GrantsReaderServiceListGrantsForEntitlementRequestValidationError{
+					field:  fmt.Sprintf("Annotations[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if len(errors) > 0 {
 		return GrantsReaderServiceListGrantsForEntitlementRequestMultiError(errors)
 	}
@@ -564,6 +598,40 @@ func (m *GrantsReaderServiceListGrantsForResourceTypeRequest) validate(all bool)
 				return err
 			}
 			errors = append(errors, err)
+		}
+
+	}
+
+	for idx, item := range m.GetAnnotations() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, GrantsReaderServiceListGrantsForResourceTypeRequestValidationError{
+						field:  fmt.Sprintf("Annotations[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, GrantsReaderServiceListGrantsForResourceTypeRequestValidationError{
+						field:  fmt.Sprintf("Annotations[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return GrantsReaderServiceListGrantsForResourceTypeRequestValidationError{
+					field:  fmt.Sprintf("Annotations[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
 		}
 
 	}
