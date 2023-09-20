08cfedc2dbdfc9f3de4f18ed91fe9c352deceee2
michael burton <michaelburton@michaels-MacBook-Pro.local>
2023-09-20 11:49:02 -0700

    linter

--- pkg/sync/expand_test.go
+++ pkg/sync/expand_test.go
@@ -14,10 +14,22 @@ func TestGetCycles(t *testing.T) {
 	graph.AddEntitlement(&v2.Entitlement{Id: "2"})
 	graph.AddEntitlement(&v2.Entitlement{Id: "3"})
 	graph.AddEntitlement(&v2.Entitlement{Id: "4"})
-	graph.AddEdge("1", "2")
-	graph.AddEdge("2", "3")
-	graph.AddEdge("3", "4")
-	graph.AddEdge("4", "2")
+	err := graph.AddEdge("1", "2")
+	if err != nil {
+		require.Empty(t, err.Error())
+	}
+	err = graph.AddEdge("2", "3")
+	if err != nil {
+		require.Empty(t, err.Error())
+	}
+	err = graph.AddEdge("3", "4")
+	if err != nil {
+		require.Empty(t, err.Error())
+	}
+	err = graph.AddEdge("4", "2")
+	if err != nil {
+		require.Empty(t, err.Error())
+	}
 	cycles, isCycle := graph.GetCycles()
 	require.True(t, isCycle)
 	require.Equal(t, [][]string{{"2", "3", "4"}}, cycles)
