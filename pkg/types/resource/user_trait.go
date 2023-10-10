926c1e6b173d3dc1e8d98761f19f6bd47febf7cc
Logan Saso <logansaso@gmail.com>
2023-10-10 15:52:53 -0700

    Added usernames and trait to user trait options

--- pkg/types/resource/user_trait.go
+++ pkg/types/resource/user_trait.go
@@ -34,6 +34,22 @@ func WithEmail(email string, primary bool) UserTraitOption {
 		return nil
 	}
 }
+func WithUsername(username string, primary bool) UserTraitOption {
+	return func(ut *v2.UserTrait) error {
+		if username == "" {
+			return nil
+		}
+
+		traitUsername := &v2.UserTrait_Username{
+			Username:  username,
+			IsPrimary: primary,
+		}
+
+		ut.Usernames = append(ut.Usernames, traitUsername)
+
+		return nil
+	}
+}
 
 func WithUserIcon(assetRef *v2.AssetRef) UserTraitOption {
 	return func(ut *v2.UserTrait) error {
