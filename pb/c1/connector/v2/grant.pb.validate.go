52d3852471abbd973656013e2da4cc6fc0cfb357
Logan Saso <logan.saso@conductorone.com>
2023-11-29 12:30:10 -0800

    Add grant as a return value in the grant response

--- pb/c1/connector/v2/grant.pb.validate.go
+++ pb/c1/connector/v2/grant.pb.validate.go
@@ -1099,6 +1099,35 @@ func (m *GrantManagerServiceGrantResponse) validate(all bool) error {
 
 	}
 
+	if all {
+		switch v := interface{}(m.GetGrant()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, GrantManagerServiceGrantResponseValidationError{
+					field:  "Grant",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, GrantManagerServiceGrantResponseValidationError{
+					field:  "Grant",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetGrant()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return GrantManagerServiceGrantResponseValidationError{
+				field:  "Grant",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return GrantManagerServiceGrantResponseMultiError(errors)
 	}
