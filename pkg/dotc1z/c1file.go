354a31445213ae855d147ddf55bc704748727ea4
Geoff Greer <geoff@greer.fm>
2024-02-16 12:42:39 -0800

    Add pragma option to c1z/c1f options.

--- pkg/dotc1z/c1file.go
+++ pkg/dotc1z/c1file.go
@@ -13,6 +13,11 @@ import (
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
 )
 
+type pragma struct {
+	name  string
+	value string
+}
+
 type C1File struct {
 	rawDb          *sql.DB
 	db             *goqu.Database
@@ -22,6 +27,7 @@ type C1File struct {
 	dbFilePath     string
 	dbUpdated      bool
 	tempDir        string
+	pragmas        []pragma
 }
 
 type C1FOption func(*C1File)
@@ -32,6 +38,12 @@ func WithC1FTmpDir(tempDir string) C1FOption {
 	}
 }
 
+func WithC1FPragma(name string, value string) C1FOption {
+	return func(o *C1File) {
+		o.pragmas = append(o.pragmas, pragma{name, value})
+	}
+}
+
 // Returns a C1File instance for the given db filepath.
 func NewC1File(ctx context.Context, dbFilePath string, opts ...C1FOption) (*C1File, error) {
 	rawDB, err := sql.Open("sqlite", dbFilePath)
@@ -39,10 +51,6 @@ func NewC1File(ctx context.Context, dbFilePath string, opts ...C1FOption) (*C1Fi
 		return nil, err
 	}
 	db := goqu.New("sqlite3", rawDB)
-	_, err = db.Exec("PRAGMA synchronous = OFF")
-	if err != nil {
-		return nil, err
-	}
 
 	c1File := &C1File{
 		rawDb:      rawDB,
@@ -63,13 +71,20 @@ func NewC1File(ctx context.Context, dbFilePath string, opts ...C1FOption) (*C1Fi
 }
 
 type c1zOptions struct {
-	tmpDir string
+	tmpDir  string
+	pragmas []pragma
 }
 type C1ZOption func(*c1zOptions)
 
 func WithTmpDir(tmpDir string) C1ZOption {
 	return func(o *c1zOptions) {
 		o.tmpDir = tmpDir
+	}
+}
+
+func WithPragma(name string, value string) C1ZOption {
+	return func(o *c1zOptions) {
+		o.pragmas = append(o.pragmas, pragma{name, value})
 	}
 }
 
@@ -85,7 +100,12 @@ func NewC1ZFile(ctx context.Context, outputFilePath string, opts ...C1ZOption) (
 		return nil, err
 	}
 
-	c1File, err := NewC1File(ctx, dbFilePath)
+	var c1fopts []C1FOption
+	for _, pragma := range options.pragmas {
+		c1fopts = append(c1fopts, WithC1FPragma(pragma.name, pragma.value))
+	}
+
+	c1File, err := NewC1File(ctx, dbFilePath, c1fopts...)
 	if err != nil {
 		return nil, err
 	}
@@ -142,6 +162,13 @@ func (c *C1File) init(ctx context.Context) error {
 		query, args := t.Schema()
 
 		_, err = c.db.ExecContext(ctx, fmt.Sprintf(query, args...))
+		if err != nil {
+			return err
+		}
+	}
+
+	for _, pragma := range c.pragmas {
+		_, err := c.db.ExecContext(ctx, fmt.Sprintf("PRAGMA %s = %s", pragma.name, pragma.value))
 		if err != nil {
 			return err
 		}
