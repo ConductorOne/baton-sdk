994775ac6239fc290154e42095a89ab1cfa0fecc
Vlada Nevyhosteny <vladimir.nevyhosteny@applifting.cz>
2024-02-20 16:07:23 +0100

    feat: httpclient with base functions

---
+++ pkg/httpclient/httpclient.go
@@ -0,0 +1,98 @@
+package httpclient
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+type (
+	HttpClient interface {
+		HttpClient() *http.Client
+		Do(req *http.Request, options ...DoOption) (*http.Response, error)
+		NewJSONRequest(ctx context.Context, method string, url *url.URL, options ...RequestOption) (*http.Request, error)
+	}
+	BaseHttpClient struct {
+		httpClient *http.Client
+	}
+
+	DoOption      func(*http.Response)
+	RequestOption func() (io.ReadWriter, map[string]string, error)
+)
+
+func WithResponse(response interface{}) DoOption {
+	return func(resp *http.Response) {
+		defer resp.Body.Close()
+		json.NewDecoder(resp.Body).Decode(response)
+	}
+}
+
+func (c *BaseHttpClient) Do(req *http.Request, options ...DoOption) (*http.Response, error) {
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, option := range options {
+		option(resp)
+	}
+
+	return resp, err
+}
+
+func WithBody(body interface{}) RequestOption {
+	return func() (io.ReadWriter, map[string]string, error) {
+		buffer := new(bytes.Buffer)
+		err := json.NewEncoder(buffer).Encode(body)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		return buffer, nil, nil
+	}
+}
+
+func WithAcceptJSONHeader() RequestOption {
+	return func() (io.ReadWriter, map[string]string, error) {
+		return nil, map[string]string{
+			"Accept": "application/json",
+		}, nil
+	}
+}
+
+func WithContentTypeJSONHeader() RequestOption {
+	return func() (io.ReadWriter, map[string]string, error) {
+		return nil, map[string]string{
+			"Content-Type": "application/json",
+		}, nil
+	}
+}
+
+func (c *BaseHttpClient) NewJSONRequest(ctx context.Context, method string, url *url.URL, options ...RequestOption) (*http.Request, error) {
+	var buffer io.ReadWriter
+	var headers map[string]string = make(map[string]string)
+	for _, option := range options {
+		buf, h, err := option()
+		if err != nil {
+			return nil, err
+		}
+
+		if buf != nil {
+			buffer = buf
+		}
+
+		for k, v := range h {
+			headers[k] = v
+		}
+	}
+
+	req, err := http.NewRequestWithContext(ctx, method, url.String(), buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
