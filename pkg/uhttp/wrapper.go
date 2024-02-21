296b450748c84b05bbe7dec1ea41fd7a78245732
vnevyhosteny <vnevyhosteny@gmail.com>
2024-02-21 19:18:44 +0100

    feat: httpclient with base functions (#104)
    * feat: httpclient with base functions
    
    * chore: move httpclient to uhttp package
    
    * chore(uhttp): wrapper tests
    
    * chore: lint

---
+++ pkg/uhttp/wrapper.go
@@ -0,0 +1,110 @@
+package uhttp
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+type (
+	HttpClient interface {
+		HttpClient() *http.Client
+		Do(req *http.Request, options ...DoOption) (*http.Response, error)
+		NewRequest(ctx context.Context, method string, url *url.URL, options ...RequestOption) (*http.Request, error)
+	}
+	BaseHttpClient struct {
+		httpClient *http.Client
+	}
+
+	DoOption      func(*http.Response) error
+	RequestOption func() (io.ReadWriter, map[string]string, error)
+)
+
+func WithJSONResponse(response interface{}) DoOption {
+	return func(resp *http.Response) error {
+		defer resp.Body.Close()
+		return json.NewDecoder(resp.Body).Decode(response)
+	}
+}
+
+func (c *BaseHttpClient) Do(req *http.Request, options ...DoOption) (*http.Response, error) {
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, option := range options {
+		err = option(resp)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return resp, err
+}
+
+func WithJSONBody(body interface{}) RequestOption {
+	return func() (io.ReadWriter, map[string]string, error) {
+		buffer := new(bytes.Buffer)
+		err := json.NewEncoder(buffer).Encode(body)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		_, headers, err := WithContentTypeJSONHeader()()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		return buffer, headers, nil
+	}
+}
+
+func WithAcceptJSONHeader() RequestOption {
+	return func() (io.ReadWriter, map[string]string, error) {
+		return nil, map[string]string{
+			"Accept": "application/json",
+		}, nil
+	}
+}
+
+func WithContentTypeJSONHeader() RequestOption {
+	return func() (io.ReadWriter, map[string]string, error) {
+		return nil, map[string]string{
+			"Content-Type": "application/json",
+		}, nil
+	}
+}
+
+func (c *BaseHttpClient) NewRequest(ctx context.Context, method string, url *url.URL, options ...RequestOption) (*http.Request, error) {
+	var buffer io.ReadWriter
+	var headers map[string]string = make(map[string]string)
+	for _, option := range options {
+		buf, h, err := option()
+		if err != nil {
+			return nil, err
+		}
+
+		if buf != nil {
+			buffer = buf
+		}
+
+		for k, v := range h {
+			headers[k] = v
+		}
+	}
+
+	req, err := http.NewRequestWithContext(ctx, method, url.String(), buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	return req, nil
+}
