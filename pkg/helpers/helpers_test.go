58b3ab481b64f172fb9d87b8f06836ae00e9a34a
Martin Albert <martin.albert.187@gmail.com>
2024-03-04 13:44:44 +0100

    Add helpers for rate limits and response content type

--- pkg/helpers/helpers_test.go
+++ pkg/helpers/helpers_test.go
@@ -1,7 +1,9 @@
 package helpers
 
 import (
+	"net/http"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -18,4 +20,42 @@ func TestHelpers_SplitFullName(t *testing.T) {
 	firstName, lastName = SplitFullName("John Jacob Jingleheimer Schmidt")
 	require.Equal(t, "John", firstName)
 	require.Equal(t, "Jacob Jingleheimer Schmidt", lastName)
+}
+
+func TestHelpers_ExtractRateLimitData(t *testing.T) {
+	n := time.Now()
+
+	resp := &http.Response{
+		Header: map[string][]string{
+			"X-Ratelimit-Limit":     {"100"},
+			"X-Ratelimit-Remaining": {"50"},
+			"X-Ratelimit-Reset":     {"60"},
+		},
+	}
+
+	rl, err := ExtractRateLimitData(resp)
+	require.NoError(t, err)
+	require.Equal(t, int64(100), rl.Limit)
+	require.Equal(t, int64(50), rl.Remaining)
+	require.Equal(t, n.Add(time.Second*60).Unix(), rl.ResetAt.AsTime().Unix())
+}
+
+func TestHelpers_IsJSONContentType_Success(t *testing.T) {
+	resp := &http.Response{
+		Header: map[string][]string{
+			"Content-Type": {"application/vdn+json"},
+		},
+	}
+	h := resp.Header.Get("Content-Type")
+	require.True(t, IsJSONContentType(h))
+}
+
+func TestHelpers_IsJSONContentType_Failure(t *testing.T) {
+	resp := &http.Response{
+		Header: map[string][]string{
+			"Content-Type": {"application/xml"},
+		},
+	}
+	h := resp.Header.Get("Content-Type")
+	require.False(t, IsJSONContentType(h))
 }
