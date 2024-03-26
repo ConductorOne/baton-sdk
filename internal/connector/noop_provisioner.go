60674eb1c6dd94faae4eb0083a8c9a92ec3d7da2
mchavez <miguel_chavez_m@hotmail.com>
2024-03-26 14:41:56 -0600

    Code review changes

--- internal/connector/noop_provisioner.go
+++ internal/connector/noop_provisioner.go
@@ -11,25 +11,25 @@ import (
 type noopProvisioner struct{}
 
 func (n *noopProvisioner) Grant(ctx context.Context, req *v2.GrantManagerServiceGrantRequest) (*v2.GrantManagerServiceGrantResponse, error) {
-	return nil, status.Error(codes.FailedPrecondition, "provisioning is not enabled")
+	return nil, status.Error(codes.FailedPrecondition, "error: provisioning is not enabled. try running with --provisioning")
 }
 
 func (n *noopProvisioner) Revoke(ctx context.Context, req *v2.GrantManagerServiceRevokeRequest) (*v2.GrantManagerServiceRevokeResponse, error) {
-	return nil, status.Error(codes.FailedPrecondition, "provisioning is not enabled")
+	return nil, status.Error(codes.FailedPrecondition, "error: provisioning is not enabled. try running with --provisioning")
 }
 
 func (n *noopProvisioner) CreateResource(ctx context.Context, request *v2.CreateResourceRequest) (*v2.CreateResourceResponse, error) {
-	return nil, status.Error(codes.FailedPrecondition, "provisioning is not enabled")
+	return nil, status.Error(codes.FailedPrecondition, "error: provisioning is not enabled. try running with --provisioning")
 }
 
 func (n *noopProvisioner) DeleteResource(ctx context.Context, request *v2.DeleteResourceRequest) (*v2.DeleteResourceResponse, error) {
-	return nil, status.Error(codes.FailedPrecondition, "provisioning is not enabled")
+	return nil, status.Error(codes.FailedPrecondition, "error: provisioning is not enabled. try running with --provisioning")
 }
 
 func (n *noopProvisioner) RotateCredential(ctx context.Context, request *v2.RotateCredentialRequest) (*v2.RotateCredentialResponse, error) {
-	return nil, status.Error(codes.FailedPrecondition, "provisioning is not enabled")
+	return nil, status.Error(codes.FailedPrecondition, "error: provisioning is not enabled. try running with --provisioning")
 }
 
 func (n *noopProvisioner) CreateAccount(ctx context.Context, request *v2.CreateAccountRequest) (*v2.CreateAccountResponse, error) {
-	return nil, status.Error(codes.FailedPrecondition, "provisioning is not enabled")
+	return nil, status.Error(codes.FailedPrecondition, "error: provisioning is not enabled. try running with --provisioning")
 }
