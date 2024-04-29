4c9dbe0f063ccd7fb14220e5f320ffd49e70669d
Vladimir Nevyhosteny <vladimir.nevyhosteny@dxheroes.io>
2024-04-29 15:33:30 +0200

    chore(uhttp): client credentials tests agains mocked server

--- pkg/uhttp/authcredentials_test.go
+++ pkg/uhttp/authcredentials_test.go
@@ -4,6 +4,9 @@ import (
 	"bytes"
 	"context"
 	"encoding/base64"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -69,17 +72,54 @@ func TestHelpers_BasicAuth_GetClient(t *testing.T) {
 }
 
 func TestHelpers_OAuth2_ClientCredentials_GetClient(t *testing.T) {
-	cfg := &clientcredentials.Config{
-		ClientID:     "test-client-id",
-		ClientSecret: "test-client-secret",
-		TokenURL:     "https://test-token-url",
+	tests := []struct {
+		tokenResponseJSON string
+		input             struct {
+			cfg *clientcredentials.Config
+		}
+		wanted struct {
+			authHeader string
+		}
+	}{
+		{
+			tokenResponseJSON: `{"access_token": "test-access-token", "token_type": "test-token-type", "expires_in": 3600}`,
+			input: struct{ cfg *clientcredentials.Config }{
+				cfg: &clientcredentials.Config{
+					ClientID:     "test-client-id",
+					ClientSecret: "test-client-secret",
+				},
+			},
+			wanted: struct {
+				authHeader string
+			}{
+				authHeader: fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte("test-client-id:test-client-secret"))),
+			},
+		},
 	}
-	cc := &OAuth2ClientCredentials{cfg: cfg}
 
-	ctx := context.Background()
-	client, err := cc.GetClient(ctx)
-	require.NoError(t, err)
-	require.NotNil(t, client)
+	for _, tt := range tests {
+		cc := &OAuth2ClientCredentials{cfg: tt.input.cfg}
+
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			require.Equal(t, "POST", r.Method)
+			require.Equal(t, tt.wanted.authHeader, r.Header.Get("Authorization"))
+
+			w.Header().Set("Content-Type", "application/json")
+			w.Write([]byte(tt.tokenResponseJSON))
+			w.WriteHeader(http.StatusOK)
+		}))
+		defer server.Close()
+		cc.cfg.TokenURL = server.URL
+
+		ctx := context.Background()
+		client, err := cc.GetClient(ctx)
+
+		require.NoError(t, err)
+		require.NotNil(t, client)
+
+		// Invoke token request
+		client.Head("https://test-url")
+	}
 }
 
 func TestHelpers_OAuth2_JWT_GetClient(t *testing.T) {
