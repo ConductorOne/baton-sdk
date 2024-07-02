b1430116b3b25f04f138577d9aeb16fbab934bd2
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-07-02 15:43:01 -0600

    Add description to default fields

--- pkg/configschema/default_schema_fields.go
+++ pkg/configschema/default_schema_fields.go
@@ -6,16 +6,17 @@ import (
 
 // defaultFields list the default fields expected in every single connector.
 var defaultFields = []ConfigField{
-	StringField("log-level", WithDefaultValue("info")),
-	StringField("log-format", WithDefaultValue(logging.LogFormatJSON)),
-	StringField("file"),
+	StringField("log-level", WithDefaultValue("info"), WithDescription("The log level: debug, info, warn, error")),
+	StringField("log-format", WithDefaultValue(logging.LogFormatJSON), WithDescription("The output format for logs: json, console")),
+	StringField("file", WithDefaultValue("sync.c1z"), WithDescription("The path to the c1z file to sync with")),
 	StringField("client-id"),
 	StringField("client-secret"),
-	StringField("grant-entitlement"),
-	StringField("grant-principal"),
-	StringField("grant-principal-type"),
-	StringField("revoke-grant"),
-	StringField("c1z-temp-dir"),
+	StringField("grant-entitlement", WithDescription("The id of the entitlement to grant to the supplied principal")),
+	StringField("grant-principal", WithDescription("The id of the resource to grant the entitlement to")),
+	StringField("grant-principal-type", WithDescription("The resource type of the principal to grant the entitlement to")),
+	StringField("revoke-grant", WithDescription("The grant to revoke")),
+	StringField("c1z-temp-dir", WithDescription("The directory to store temporary files in. It "+
+		"must exist, and write access is required. Defaults to the OS temporary directory.")), // hidden
 }
 
 func ensureDefaultFieldsExists(originalFields []ConfigField) []ConfigField {
