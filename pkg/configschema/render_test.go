6ee7b4e4503a25319b39620db6c4f68302d805c1
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-07-01 18:21:19 -0600

    Render templates with field schema
    create source code for configuring a connector and also configures the
    Cobra command-line for the connector too

---
+++ pkg/configschema/render_test.go
@@ -0,0 +1,64 @@
+package configschema
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var renderConfigExpected = `// THIS FILE WAS AUTO GENERATED. DO NOT EDIT
+package main
+
+type Configuration struct {
+	Field1String string
+	// this is a description
+	Field2Int int
+}
+`
+
+var renderCLIExpected = `// THIS FILE WAS AUTO GENERATED. DO NOT EDIT
+package main
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func NewCLI(cmd *cobra.Command) {
+	cmd.PersistentFlags().String("field-1-string",
+		"value",
+		"($BATON_FIELD_1_STRING)")
+	cmd.PersistentFlags().Int("field-2-int",
+		0,
+		"this is a description ($BATON_FIELD_2_INT)")
+
+}
+`
+
+func TestRenderConfig(t *testing.T) {
+	fields := []ConfigField{
+		StringField("field-1-string", WithDefaultValue("value")),
+		IntField("field-2-int", WithDescription("this is a description")),
+	}
+
+	b := bytes.NewBuffer(nil)
+
+	err := RenderConfig(TemplateData{PackageName: "main", Fields: fields}, b)
+	require.NoError(t, err)
+
+	require.Equal(t, renderConfigExpected, b.String())
+}
+
+func TestRenderCobra(t *testing.T) {
+	fields := []ConfigField{
+		StringField("field-1-string", WithDefaultValue("value")),
+		IntField("field-2-int", WithDescription("this is a description")),
+	}
+
+	b := bytes.NewBuffer(nil)
+
+	err := RenderCLI(TemplateData{PackageName: "main", Fields: fields}, b)
+	require.NoError(t, err)
+
+	require.Equal(t, renderCLIExpected, b.String())
+}
