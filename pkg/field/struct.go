b9ee01a9bf49f2ab9178852a320edc34d2d217dd
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-07-12 13:32:33 -0600

    Add missing helper function to create Configuration structs

--- pkg/field/struct.go
+++ pkg/field/struct.go
@@ -4,3 +4,10 @@ type Configuration struct {
 	Fields      []SchemaField
 	Constraints []SchemaFieldRelationship
 }
+
+func NewConfiguration(fields []SchemaField, constraints ...SchemaFieldRelationship) Configuration {
+	return Configuration{
+		Fields:      fields,
+		Constraints: constraints,
+	}
+}
