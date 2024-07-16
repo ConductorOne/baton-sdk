59334756c793f5bddac1854c32b707beb706ec08
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-07-16 13:05:24 -0600

    Mark flags as required if they are

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -114,6 +114,23 @@ func DefineConfiguration(
 				)
 			}
 		}
+
+		// mark required
+		if field.Required {
+			if field.FieldType == reflect.Bool {
+				return nil, nil, fmt.Errorf("requiring %s of type %s does not make sense", field.FieldName, field.FieldType)
+			}
+
+			err := mainCMD.MarkFlagRequired(field.FieldName)
+			if err != nil {
+				return nil, nil, fmt.Errorf(
+					"cannot require field %s, %s: %w",
+					field.FieldName,
+					field.FieldType,
+					err,
+				)
+			}
+		}
 	}
 
 	// apply constrains
