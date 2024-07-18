95e48d0f11aead0d56a23b5bffe87d02cc38cb23
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-07-18 15:40:37 -0600

    Use context.WithTimeout to avoid full execution of dummy connector

--- internal/tests/dummymain.go
+++ internal/tests/dummymain.go
@@ -2,7 +2,9 @@ package tests
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"time"
 
 	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
@@ -12,7 +14,8 @@ import (
 )
 
 func entrypoint(cfg field.Configuration) (*viper.Viper, error) {
-	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*20)
+	defer cancel()
 
 	v, cmd, err := config.DefineConfiguration(ctx, "baton-dummy", getConnector, cfg)
 	if err != nil {
@@ -23,6 +26,10 @@ func entrypoint(cfg field.Configuration) (*viper.Viper, error) {
 
 	err = cmd.Execute()
 	if err != nil {
+		// we don't want a full execution
+		if errors.Is(err, context.DeadlineExceeded) {
+			return v, nil
+		}
 		return nil, fmt.Errorf("(Cobra) Execute failed: %w", err)
 	}
 	return v, nil
