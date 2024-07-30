b1d7a8bc61a20bce7bbb3872ea05f609b01b42d9
mchavez <miguel_chavez_m@hotmail.com>
2024-07-29 18:10:34 -0600

    Refactoring code

--- pkg/uhttp/gocache.go
+++ pkg/uhttp/gocache.go
@@ -18,50 +18,17 @@ import (
 )
 
 type GoCache struct {
-	ttl         time.Duration
 	rootLibrary *bigcache.BigCache
 }
 
 func NewGoCache(ctx context.Context, ttl int32, cacheMaxSize int, isLogLevel bool) (GoCache, error) {
 	l := ctxzap.Extract(ctx)
-	config := bigcache.Config{
-		// number of shards (must be a power of 2)
-		Shards: 1024,
-
-		// time after which entry can be evicted
-		LifeWindow: time.Duration(ttl) * time.Second,
-
-		// Interval between removing expired entries (clean up).
-		// If set to <= 0 then no action is performed.
-		// Setting to < 1 second is counterproductive — bigcache has a one second resolution.
-		CleanWindow: 5 * time.Minute,
-
-		// rps * lifeWindow, used only in initial memory allocation
-		MaxEntriesInWindow: 1000 * 10 * 60,
-
-		// max entry size in bytes, used only in initial memory allocation
-		MaxEntrySize: 500,
-
-		// prints information about additional memory allocation
-		Verbose: isLogLevel,
-
-		// cache will not allocate more memory than this limit, value in MB
-		// if value is reached then the oldest entries can be overridden for the new ones
-		// 0 value means no size limit
-		// Default value "GB eq 2048MB
-		HardMaxCacheSize: cacheMaxSize, // BATON_CACHE_MAX_SIZE
-
-		// callback fired when the oldest entry is removed because of its expiration time or no space left
-		// for the new entry, or because delete was called. A bitmask representing the reason will be returned.
-		// Default value is nil which means no callback and it prevents from unwrapping the oldest entry.
-		OnRemove: nil,
-
-		// OnRemoveWithReason is a callback fired when the oldest entry is removed because of its expiration time or no space left
-		// for the new entry, or because delete was called. A constant representing the reason will be passed through.
-		// Default value is nil which means no callback and it prevents from unwrapping the oldest entry.
-		// Ignored if OnRemove is specified.
-		OnRemoveWithReason: nil,
-	}
+	config := bigcache.DefaultConfig(time.Duration(ttl) * time.Second)
+	// prints information about additional memory allocation
+	config.Verbose = isLogLevel
+	// cache will not allocate more memory than this limit, value in MB
+	// 0 value means no size limit
+	config.HardMaxCacheSize = cacheMaxSize
 	cache, initErr := bigcache.New(ctx, config)
 	if initErr != nil {
 		l.Error("in-memory cache error", zap.Any("NewGoCache", initErr))
@@ -70,7 +37,6 @@ func NewGoCache(ctx context.Context, ttl int32, cacheMaxSize int, isLogLevel boo
 
 	l.Debug("in-memory cache config", zap.Any("config", config))
 	gc := GoCache{
-		ttl:         config.LifeWindow,
 		rootLibrary: cache,
 	}
 
