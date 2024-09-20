cfcd6c367c6836de8ce1b26bb7f58a3f9815a1cc
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-09-19 19:36:27 -0600

    Rename test and add 2 test cases

--- internal/tests/notrequireargsinsubcommands_test.go
+++ internal/tests/notrequireargsinsubcommands_test.go
@@ -8,19 +8,29 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCallSubCommandHelp(t *testing.T) {
+func TestCallSubCommand(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeoutIn)
 	defer cancel()
 
 	requiredField := field.StringField("name", field.WithRequired(true))
+	carrier := field.NewConfiguration(
+		[]field.SchemaField{
+			requiredField,
+		},
+	)
 
-	t.Run("should run help sub-command successfully", func(t *testing.T) {
-		carrier := field.NewConfiguration(
-			[]field.SchemaField{
-				requiredField,
-			},
-		)
+	t.Run("should run «help» sub-command successfully", func(t *testing.T) {
 		_, err := entrypoint(ctx, carrier, "help")
+		require.NoError(t, err)
+	})
+
+	t.Run("should run «capabilities» sub-command without success", func(t *testing.T) {
+		_, err := entrypoint(ctx, carrier, "capabilities")
+		require.Error(t, err)
+	})
+
+	t.Run("should run «completion zsh» sub-command successfully", func(t *testing.T) {
+		_, err := entrypoint(ctx, carrier, "completion", "zsh")
 		require.NoError(t, err)
 	})
 }
