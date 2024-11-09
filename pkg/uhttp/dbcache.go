d236ab6fa1fdf1f44721f9070c9ea167451dcdcf
Geoff Greer <geoff@greer.fm>
2024-11-08 17:30:38 -0800

    Lots of DB cache cleanup. Use higher log levels for errors. Simplify functions.

--- pkg/uhttp/dbcache.go
+++ pkg/uhttp/dbcache.go
@@ -60,15 +60,15 @@ func (b *SqliteError) Error() string {
 
 const (
 	failStartTransaction       = "Failed to start a transaction"
-	nilConnection              = "Database connection is nil"
 	errQueryingTable           = "Error querying cache table"
 	failRollback               = "Failed to rollback transaction"
 	failInsert                 = "Failed to insert response data into cache table"
-	staticQuery                = "INSERT INTO http_stats(key, %s) values(?, 1)"
 	failScanResponse           = "Failed to scan rows for cached response"
 	cacheTTLThreshold          = 60
 	cacheTTLMultiplier   int64 = 5
 )
+
+var errNilConnection = errors.New("database connection is nil")
 
 var defaultWaitDuration = cacheTTLThreshold * time.Second // Default Cleanup interval, 60 seconds
 
@@ -79,14 +79,15 @@ func NewDBCache(ctx context.Context, cfg CacheConfig) (*DBCache, error) {
 	var (
 		err error
 		dc  = &DBCache{
-			waitDuration: defaultWaitDuration, // Default Cleanup interval, 60 seconds
-			stats:        cfg.LogDebug,
+			waitDuration:   defaultWaitDuration, // Default Cleanup interval, 60 seconds
+			stats:          true,
+			expirationTime: time.Duration(cfg.TTL) * time.Second,
 		}
 	)
 	l := ctxzap.Extract(ctx)
 	dc, err = dc.load(ctx)
 	if err != nil {
-		l.Debug("Failed to open database", zap.Error(err))
+		l.Warn("Failed to open database", zap.Error(err))
 		return nil, err
 	}
 
@@ -111,7 +112,7 @@ func NewDBCache(ctx context.Context, cfg CacheConfig) (*DBCache, error) {
 	DELETE FROM http_cache;
 	DELETE FROM http_stats;`)
 	if err != nil {
-		l.Debug("Failed to create cache table in database", zap.Error(err))
+		l.Warn("Failed to create cache table in database", zap.Error(err))
 		return nil, err
 	}
 
@@ -123,8 +124,6 @@ func NewDBCache(ctx context.Context, cfg CacheConfig) (*DBCache, error) {
 	if cfg.TTL > cacheTTLThreshold {
 		dc.waitDuration = time.Duration(cfg.TTL*cacheTTLMultiplier) * time.Second // set as a fraction of the Cache TTL
 	}
-
-	dc.expirationTime = time.Duration(cfg.TTL) * time.Second // time for removing expired key
 
 	go func(waitDuration, expirationTime time.Duration) {
 		ctxWithTimeout, cancel := context.WithTimeout(
@@ -142,13 +141,13 @@ func NewDBCache(ctx context.Context, cfg CacheConfig) (*DBCache, error) {
 				ticker.Stop()
 				err := dc.cleanup(ctx)
 				if err != nil {
-					l.Debug("shutting down cache failed", zap.Error(err))
+					l.Warn("shutting down cache failed", zap.Error(err))
 				}
 				return
 			case <-ticker.C:
-				err := dc.deleteExpired(ctx)
+				err := dc.remove(ctx)
 				if err != nil {
-					l.Debug("Failed to delete expired cache entries", zap.Error(err))
+					l.Warn("Failed to delete expired cache entries", zap.Error(err))
 				}
 			}
 		}
@@ -161,41 +160,39 @@ func (d *DBCache) load(ctx context.Context) (*DBCache, error) {
 	l := ctxzap.Extract(ctx)
 	cacheDir, err := os.UserCacheDir()
 	if err != nil {
-		l.Debug("Failed to read user cache directory", zap.Error(err))
+		l.Warn("Failed to read user cache directory", zap.Error(err))
 		return nil, err
 	}
 
 	file := filepath.Join(cacheDir, "lcache.db")
 	d.location = file
 
+	l.Debug("Opening database", zap.String("location", file))
 	rawDB, err := sql.Open("sqlite", file)
 	if err != nil {
 		return nil, err
 	}
+	l.Debug("Opened database", zap.String("location", file))
 
 	d.db = goqu.New("sqlite3", rawDB)
 	d.rawDb = rawDB
 	return d, nil
 }
 
-func checkFileExists(filePath string) bool {
-	_, err := os.Stat(filePath)
-	return !errors.Is(err, os.ErrNotExist)
-}
-
 func (d *DBCache) removeDB(ctx context.Context) error {
-	if !checkFileExists(d.location) {
+	_, err := os.Stat(d.location)
+	if errors.Is(err, os.ErrNotExist) {
 		return fmt.Errorf("file not found %s", d.location)
 	}
 
-	err := d.close(ctx)
+	err = d.close(ctx)
 	if err != nil {
 		return err
 	}
 	// TODO: close DB so no file handles exist and we can delete the file on windows
 	err = os.Remove(d.location)
 	if err != nil {
-		ctxzap.Extract(ctx).Debug("error removing database", zap.Error(err))
+		ctxzap.Extract(ctx).Warn("error removing cache database", zap.Error(err))
 		return err
 	}
 
@@ -208,10 +205,6 @@ func (d *DBCache) Get(req *http.Request) (*http.Response, error) {
 		isFound bool = false
 		resp    *http.Response
 	)
-	if d.IsNilConnection() {
-		return nil, fmt.Errorf("%s", nilConnection)
-	}
-
 	key, err := CreateCacheKey(req)
 	if err != nil {
 		return nil, err
@@ -231,15 +224,15 @@ func (d *DBCache) Get(req *http.Request) (*http.Response, error) {
 
 	if d.stats {
 		if isFound {
-			err = d.hits(ctx, key)
+			err = d.updateStats(ctx, "hits", key)
 			if err != nil {
-				ctxzap.Extract(ctx).Debug("Failed to update cache hits", zap.Error(err))
+				ctxzap.Extract(ctx).Warn("Failed to update cache hits", zap.Error(err))
 			}
 		}
 
-		err = d.misses(ctx, key)
+		err = d.updateStats(ctx, "misses", key)
 		if err != nil {
-			ctxzap.Extract(ctx).Debug("Failed to update cache misses", zap.Error(err))
+			ctxzap.Extract(ctx).Warn("Failed to update cache misses", zap.Error(err))
 		}
 	}
 
@@ -252,16 +245,12 @@ func (d *DBCache) Set(req *http.Request, value *http.Response) error {
 	if err != nil {
 		return err
 	}
-	var url string
-	if d.IsNilConnection() {
-		return fmt.Errorf("%s", nilConnection)
-	}
-
 	cacheableResponse, err := httputil.DumpResponse(value, true)
 	if err != nil {
 		return err
 	}
 
+	var url string
 	if value.Request != nil {
 		url = value.Request.URL.String()
 	}
@@ -279,31 +268,15 @@ func (d *DBCache) Set(req *http.Request, value *http.Response) error {
 }
 
 func (d *DBCache) cleanup(ctx context.Context) error {
-	if d.IsNilConnection() {
-		return fmt.Errorf("%s", nilConnection)
-	}
-
 	l := ctxzap.Extract(ctx)
 	stats, err := d.getStats(ctx)
 	if err != nil {
-		l.Debug("error getting stats", zap.Error(err))
+		l.Warn("error getting stats", zap.Error(err))
 		return err
 	}
 
 	l.Debug("summary and stats", zap.Any("stats", stats))
-	err = d.close(ctx)
-	if err != nil {
-		l.Debug("error closing db", zap.Error(err))
-		return err
-	}
-
-	err = d.removeDB(ctx)
-	if err != nil {
-		l.Debug("error removing db", zap.Error(err))
-		return err
-	}
-
-	return nil
+	return d.removeDB(ctx)
 }
 
 // Insert data into the cache table.
@@ -313,22 +286,22 @@ func (d *DBCache) insert(ctx context.Context, key string, value any, url string)
 		err   error
 		ok    bool
 	)
-	if d.IsNilConnection() {
-		return fmt.Errorf("%s", nilConnection)
+	if d.db == nil {
+		return errNilConnection
 	}
 
 	l := ctxzap.Extract(ctx)
 	if bytes, ok = value.([]byte); !ok {
 		bytes, err = json.Marshal(value)
 		if err != nil {
-			l.Debug("Failed to marshal response data", zap.Error(err))
+			l.Warn("Failed to marshal response data", zap.Error(err))
 			return err
 		}
 	}
 
 	tx, err := d.db.Begin()
 	if err != nil {
-		l.Debug(failStartTransaction, zap.Error(err))
+		l.Warn(failStartTransaction, zap.Error(err))
 		return err
 	}
 
@@ -341,54 +314,50 @@ func (d *DBCache) insert(ctx context.Context, key string, value any, url string)
 		},
 	)
 	ds = ds.OnConflict(goqu.DoUpdate("key", CacheRow{
+		Key:     key,
 		Value:   bytes,
 		Expires: time.Now().Add(d.expirationTime),
 		Url:     url,
 	}))
 	insertSQL, args, err := ds.ToSQL()
 	if err != nil {
-		l.Debug("Failed to create insert statement", zap.Error(err))
+		l.Warn("Failed to create insert statement", zap.Error(err))
 		return err
 	}
 	_, err = tx.ExecContext(ctx, insertSQL, args...)
 	if err != nil {
 		if errtx := tx.Rollback(); errtx != nil {
-			l.Debug(failRollback, zap.Error(errtx))
+			l.Warn(failRollback, zap.Error(errtx))
 		}
 
-		l.Debug(failInsert, zap.Error(err))
+		l.Warn(failInsert, zap.Error(err), zap.String("sql", insertSQL))
 		return err
 	}
 
 	err = tx.Commit()
 	if err != nil {
 		if errtx := tx.Rollback(); errtx != nil {
-			l.Debug(failRollback, zap.Error(errtx))
+			l.Warn(failRollback, zap.Error(errtx))
 		}
 
-		l.Debug(failInsert, zap.Error(err))
+		l.Warn(failInsert, zap.Error(err), zap.String("sql", insertSQL))
 		return err
 	}
 
 	return nil
 }
 
-// IsNilConnection check if the database connection is nil.
-func (d *DBCache) IsNilConnection() bool {
-	return d.db == nil
-}
-
 // pick query for cached response.
 func (d *DBCache) pick(ctx context.Context, key string) ([]byte, error) {
 	var data []byte
-	if d.IsNilConnection() {
-		return nil, fmt.Errorf("%s", nilConnection)
+	if d.db == nil {
+		return nil, errNilConnection
 	}
 
 	l := ctxzap.Extract(ctx)
 	rows, err := d.db.QueryContext(ctx, "SELECT value FROM http_cache where key = ?", key)
 	if err != nil {
-		l.Debug(errQueryingTable, zap.Error(err))
+		l.Warn(errQueryingTable, zap.Error(err))
 		return nil, err
 	}
 
@@ -396,7 +365,7 @@ func (d *DBCache) pick(ctx context.Context, key string) ([]byte, error) {
 	for rows.Next() {
 		err = rows.Scan(&data)
 		if err != nil {
-			l.Debug(failScanResponse, zap.Error(err))
+			l.Warn(failScanResponse, zap.Error(err))
 			return nil, err
 		}
 	}
@@ -404,35 +373,36 @@ func (d *DBCache) pick(ctx context.Context, key string) ([]byte, error) {
 	return data, nil
 }
 
+// Delete all expired items from the cache.
 func (d *DBCache) remove(ctx context.Context) error {
-	if d.IsNilConnection() {
-		return fmt.Errorf("%s", nilConnection)
+	if d.db == nil {
+		return errNilConnection
 	}
 
 	l := ctxzap.Extract(ctx)
 	tx, err := d.db.Begin()
 	if err != nil {
-		l.Debug(failStartTransaction, zap.Error(err))
+		l.Warn(failStartTransaction, zap.Error(err))
 		return err
 	}
 
 	_, err = d.db.ExecContext(ctx, "DELETE FROM http_cache WHERE expires < ?", time.Now().UnixNano())
 	if err != nil {
 		if errtx := tx.Rollback(); errtx != nil {
-			l.Debug(failRollback, zap.Error(errtx))
+			l.Warn(failRollback, zap.Error(errtx))
 		}
 
-		l.Debug("Failed to delete cache key", zap.Error(err))
+		l.Warn("Failed to delete expired cache entries", zap.Error(err))
 		return err
 	}
 
 	err = tx.Commit()
 	if err != nil {
 		if errtx := tx.Rollback(); errtx != nil {
-			l.Debug(failRollback, zap.Error(errtx))
+			l.Warn(failRollback, zap.Error(errtx))
 		}
 
-		l.Debug("Failed to remove cache entry", zap.Error(err))
+		l.Warn("Failed to commit after deleting expired cache entries", zap.Error(err))
 		return err
 	}
 
@@ -440,98 +410,51 @@ func (d *DBCache) remove(ctx context.Context) error {
 }
 
 func (d *DBCache) close(ctx context.Context) error {
-	if d.IsNilConnection() {
-		return fmt.Errorf("%s", nilConnection)
+	if d.db == nil {
+		return errNilConnection
 	}
 
 	err := d.rawDb.Close()
 	if err != nil {
-		ctxzap.Extract(ctx).Debug("Failed to close database connection", zap.Error(err))
+		ctxzap.Extract(ctx).Warn("Failed to close database connection", zap.Error(err))
 		return err
 	}
 
 	return nil
 }
 
-// Delete all expired items from the cache.
-func (d *DBCache) deleteExpired(ctx context.Context) error {
-	if d.IsNilConnection() {
-		return fmt.Errorf("%s", nilConnection)
+func (d *DBCache) updateStats(ctx context.Context, field, key string) error {
+	if d.db == nil {
+		return errNilConnection
 	}
-
-	l := ctxzap.Extract(ctx)
-	err := d.remove(ctx)
-	if err != nil {
-		l.Debug("error removing rows", zap.Error(err))
-	}
-
-	return nil
-}
-
-func (d *DBCache) hits(ctx context.Context, key string) error {
-	if d.IsNilConnection() {
-		return fmt.Errorf("%s", nilConnection)
-	}
-
-	strField := "hits"
-	err := d.update(ctx, strField, key)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (d *DBCache) misses(ctx context.Context, key string) error {
-	if d.IsNilConnection() {
-		return fmt.Errorf("%s", nilConnection)
-	}
-
-	strField := "misses"
-	err := d.update(ctx, strField, key)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (d *DBCache) update(ctx context.Context, field, key string) error {
 	l := ctxzap.Extract(ctx)
 	tx, err := d.db.Begin()
 	if err != nil {
-		l.Debug(failStartTransaction, zap.Error(err))
+		l.Warn(failStartTransaction, zap.Error(err))
 		return err
 	}
 
-	query, args := d.queryString(field)
-	_, err = d.db.ExecContext(ctx, fmt.Sprintf(query, args...), key)
+	_, err = d.db.ExecContext(ctx, fmt.Sprintf("INSERT INTO http_stats(key, %s) values(?, 1)", field), key)
 	if err != nil {
 		if errtx := tx.Rollback(); errtx != nil {
-			l.Debug(failRollback, zap.Error(errtx))
+			l.Warn(failRollback, zap.Error(errtx))
 		}
 
-		l.Debug("error updating "+field, zap.Error(err))
+		l.Warn("error updating "+field, zap.Error(err))
 		return err
 	}
 
 	err = tx.Commit()
 	if err != nil {
 		if errtx := tx.Rollback(); errtx != nil {
-			l.Debug(failRollback, zap.Error(errtx))
+			l.Warn(failRollback, zap.Error(errtx))
 		}
 
-		l.Debug("Failed to update "+field, zap.Error(err))
+		l.Warn("Failed to update "+field, zap.Error(err))
 		return err
 	}
 
 	return nil
-}
-
-func (d *DBCache) queryString(field string) (string, []interface{}) {
-	return staticQuery, []interface{}{
-		fmt.Sprint(field),
-	}
 }
 
 func (d *DBCache) getStats(ctx context.Context) (CacheStats, error) {
@@ -539,8 +462,8 @@ func (d *DBCache) getStats(ctx context.Context) (CacheStats, error) {
 		hits   int64
 		misses int64
 	)
-	if d.IsNilConnection() {
-		return CacheStats{}, fmt.Errorf("%s", nilConnection)
+	if d.db == nil {
+		return CacheStats{}, errNilConnection
 	}
 
 	l := ctxzap.Extract(ctx)
