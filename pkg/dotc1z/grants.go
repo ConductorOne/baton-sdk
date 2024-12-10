33cd28b71435a3d40c7144c7c243ab04d3a6badc
Geoff Greer <geoff@greer.fm>
2024-12-09 21:43:15 -0800

    Add index on entitlement + principal to speed up grant expansion. (#258)

--- pkg/dotc1z/grants.go
+++ pkg/dotc1z/grants.go
@@ -29,6 +29,7 @@ create table if not exists %s (
 create index if not exists %s on %s (resource_type_id, resource_id);
 create index if not exists %s on %s (entitlement_id);
 create index if not exists %s on %s (principal_resource_type_id, principal_resource_id);
+create index if not exists %s on %s (entitlement_id, principal_resource_type_id, principal_resource_id);
 create unique index if not exists %s on %s (external_id, sync_id);`
 
 var grants = (*grantsTable)(nil)
@@ -51,6 +52,8 @@ func (r *grantsTable) Schema() (string, []interface{}) {
 		fmt.Sprintf("idx_grants_entitlement_id_v%s", r.Version()),
 		r.Name(),
 		fmt.Sprintf("idx_grants_principal_id_v%s", r.Version()),
+		r.Name(),
+		fmt.Sprintf("idx_grants_entitlement_id_principal_id_v%s", r.Version()),
 		r.Name(),
 		fmt.Sprintf("idx_grants_external_sync_v%s", r.Version()),
 		r.Name(),
