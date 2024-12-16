9d020b0644cab22c82efa5e19f5da24a3804322f
Geoff Greer <geoff@greer.fm>
2024-12-16 11:54:56 -0800

    Treat errors syncing grants or entitlements as warnings. Fail if we hit too many warnings.

--- pkg/sync/syncer.go
+++ pkg/sync/syncer.go
@@ -146,6 +146,20 @@ func shouldWaitAndRetry(ctx context.Context, err error) bool {
 	}
 }
 
+func isWarning(ctx context.Context, err error) bool {
+	if err == nil {
+		return false
+	}
+
+	if status.Code(err) == codes.NotFound {
+		return true
+	}
+
+	return false
+}
+
+var warnings []error
+
 // Sync starts the syncing process. The sync process is driven by the action stack that is part of the state object.
 // For each page of data that is required to be fetched from the connector, a new action is pushed on to the stack. Once
 // an action is completed, it is popped off of the queue. Before processing each action, we checkpoint the state object
@@ -205,6 +219,10 @@ func (s *syncer) Sync(ctx context.Context) error {
 			return err
 		}
 
+		// TODO: count actions divided by warnings and error if warning percentage is too high
+		if len(warnings) > 10 {
+			return fmt.Errorf("too many warnings, exiting sync. warnings: %v", warnings)
+		}
 		select {
 		case <-runCtx.Done():
 			err = context.Cause(runCtx)
@@ -254,6 +272,12 @@ func (s *syncer) Sync(ctx context.Context) error {
 
 		case SyncEntitlementsOp:
 			err = s.SyncEntitlements(ctx)
+			if isWarning(ctx, err) {
+				l.Error("skipping entitlement", zap.Any("state", s.state.Current()), zap.Error(err))
+				warnings = append(warnings, err)
+				s.state.FinishAction(ctx)
+				continue
+			}
 			if !shouldWaitAndRetry(ctx, err) {
 				return err
 			}
@@ -261,6 +285,12 @@ func (s *syncer) Sync(ctx context.Context) error {
 
 		case SyncGrantsOp:
 			err = s.SyncGrants(ctx)
+			if isWarning(ctx, err) {
+				l.Error("skipping grant", zap.Any("state", s.state.Current()), zap.Error(err))
+				warnings = append(warnings, err)
+				s.state.FinishAction(ctx)
+				continue
+			}
 			if !shouldWaitAndRetry(ctx, err) {
 				return err
 			}
