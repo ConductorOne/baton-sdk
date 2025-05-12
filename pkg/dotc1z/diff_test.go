b52cde4cdea5d7e46ec0468f41757ce74803945a
Logan Saso <logan.saso@conductorone.com>
2025-05-12 15:20:15 -0700

    Don't close the file before we're done using it

--- pkg/dotc1z/diff_test.go
+++ pkg/dotc1z/diff_test.go
@@ -13,28 +13,26 @@ import (
 func TestGenerateSyncDiff(t *testing.T) {
 	ctx := context.Background()
 
-	// Create paths for our test files
-	baseFilePath := filepath.Join(c1zTests.workingDir, "base.c1z")
-	newFilePath := filepath.Join(c1zTests.workingDir, "new.c1z")
+	filePath := filepath.Join(c1zTests.workingDir, "file.c1z")
 
 	// Create the base C1Z file
-	baseFile, err := NewC1ZFile(ctx, baseFilePath)
+	syncFile, err := NewC1ZFile(ctx, filePath, WithPragma("journal_mode", "WAL"))
 	require.NoError(t, err)
+	defer syncFile.Close()
 
 	// Start a sync in the base file
-	baseSyncID, newSync, err := baseFile.StartSync(ctx)
+	baseSyncID, err := syncFile.StartNewSyncV2(ctx, string(SyncTypeFull), "")
 	require.NoError(t, err)
-	require.True(t, newSync)
 	require.NotEmpty(t, baseSyncID)
 
 	// Add a resource type to the base file
 	resourceTypeID := testResourceType
-	err = baseFile.PutResourceTypes(ctx, &v2.ResourceType{Id: resourceTypeID})
+	err = syncFile.PutResourceTypes(ctx, &v2.ResourceType{Id: resourceTypeID})
 	require.NoError(t, err)
 
 	// Add a resource to the base file
 	baseResourceID := "base-resource"
-	err = baseFile.PutResources(ctx, &v2.Resource{
+	err = syncFile.PutResources(ctx, &v2.Resource{
 		Id: &v2.ResourceId{
 			ResourceType: resourceTypeID,
 			Resource:     baseResourceID,
@@ -43,29 +41,20 @@ func TestGenerateSyncDiff(t *testing.T) {
 	require.NoError(t, err)
 
 	// End the sync in the base file
-	err = baseFile.EndSync(ctx)
-	require.NoError(t, err)
-
-	// Close the base file
-	err = baseFile.Close()
-	require.NoError(t, err)
-
-	// Create the new C1Z file
-	newFile, err := NewC1ZFile(ctx, newFilePath)
+	err = syncFile.EndSync(ctx)
 	require.NoError(t, err)
 
 	// Start a sync in the new file
-	newSyncID, newSync, err := newFile.StartSync(ctx)
+	newSyncID, err := syncFile.StartNewSyncV2(ctx, string(SyncTypeFull), "")
 	require.NoError(t, err)
-	require.True(t, newSync)
 	require.NotEmpty(t, newSyncID)
 
 	// Add the same resource type to the new file
-	err = newFile.PutResourceTypes(ctx, &v2.ResourceType{Id: resourceTypeID})
+	err = syncFile.PutResourceTypes(ctx, &v2.ResourceType{Id: resourceTypeID})
 	require.NoError(t, err)
 
 	// Add the same resource to the new file
-	err = newFile.PutResources(ctx, &v2.Resource{
+	err = syncFile.PutResources(ctx, &v2.Resource{
 		Id: &v2.ResourceId{
 			ResourceType: resourceTypeID,
 			Resource:     baseResourceID,
@@ -75,7 +64,7 @@ func TestGenerateSyncDiff(t *testing.T) {
 
 	// Add a new resource to the new file that wasn't in the base file
 	newResourceID := "new-resource"
-	err = newFile.PutResources(ctx, &v2.Resource{
+	err = syncFile.PutResources(ctx, &v2.Resource{
 		Id: &v2.ResourceId{
 			ResourceType: resourceTypeID,
 			Resource:     newResourceID,
@@ -84,20 +73,20 @@ func TestGenerateSyncDiff(t *testing.T) {
 	require.NoError(t, err)
 
 	// End the sync in the new file
-	err = newFile.EndSync(ctx)
+	err = syncFile.EndSync(ctx)
 	require.NoError(t, err)
 
 	// Generate a diff between the two files
-	diffSyncID, err := newFile.GenerateSyncDiff(ctx, baseSyncID, newSyncID)
+	diffSyncID, err := syncFile.GenerateSyncDiff(ctx, baseSyncID, newSyncID)
 	require.NoError(t, err)
 	require.NotEmpty(t, diffSyncID)
 
 	// Set the view sync ID to the diff sync ID
-	err = newFile.ViewSync(ctx, diffSyncID)
+	err = syncFile.ViewSync(ctx, diffSyncID)
 	require.NoError(t, err)
 
 	// Verify that the diff contains only the new resource
-	resourcesResp, err := newFile.ListResources(ctx, &v2.ResourcesServiceListResourcesRequest{
+	resourcesResp, err := syncFile.ListResources(ctx, &v2.ResourcesServiceListResourcesRequest{
 		ResourceTypeId: resourceTypeID,
 	})
 	require.NoError(t, err)
@@ -105,6 +94,6 @@ func TestGenerateSyncDiff(t *testing.T) {
 	require.Equal(t, newResourceID, resourcesResp.List[0].Id.Resource)
 
 	// Close the new file
-	err = newFile.Close()
+	err = syncFile.Close()
 	require.NoError(t, err)
 }
