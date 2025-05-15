cf8445fb98981f7162186e59bbab03fe4f124aa1
MJ Palanker <mj.palanker@conductorone.com>
2025-05-15 15:54:49 -0700

    listEventFeeds protos

--- pb/c1/connector/v2/event_feed.pb.validate.go
+++ pb/c1/connector/v2/event_feed.pb.validate.go
@@ -432,6 +432,357 @@ var _ interface {
 	ErrorName() string
 } = ListEventsResponseValidationError{}
 
+// Validate checks the field values on ListEventFeedsRequest with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *ListEventFeedsRequest) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on ListEventFeedsRequest with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// ListEventFeedsRequestMultiError, or nil if none found.
+func (m *ListEventFeedsRequest) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *ListEventFeedsRequest) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if m.GetPageSize() != 0 {
+
+		if m.GetPageSize() > 250 {
+			err := ListEventFeedsRequestValidationError{
+				field:  "PageSize",
+				reason: "value must be less than or equal to 250",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+	}
+
+	if m.GetPageToken() != "" {
+
+		if l := len(m.GetPageToken()); l < 1 || l > 1048576 {
+			err := ListEventFeedsRequestValidationError{
+				field:  "PageToken",
+				reason: "value length must be between 1 and 1048576 bytes, inclusive",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+	}
+
+	for idx, item := range m.GetAnnotations() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, ListEventFeedsRequestValidationError{
+						field:  fmt.Sprintf("Annotations[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, ListEventFeedsRequestValidationError{
+						field:  fmt.Sprintf("Annotations[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return ListEventFeedsRequestValidationError{
+					field:  fmt.Sprintf("Annotations[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	if len(errors) > 0 {
+		return ListEventFeedsRequestMultiError(errors)
+	}
+
+	return nil
+}
+
+// ListEventFeedsRequestMultiError is an error wrapping multiple validation
+// errors returned by ListEventFeedsRequest.ValidateAll() if the designated
+// constraints aren't met.
+type ListEventFeedsRequestMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m ListEventFeedsRequestMultiError) Error() string {
+	msgs := make([]string, 0, len(m))
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m ListEventFeedsRequestMultiError) AllErrors() []error { return m }
+
+// ListEventFeedsRequestValidationError is the validation error returned by
+// ListEventFeedsRequest.Validate if the designated constraints aren't met.
+type ListEventFeedsRequestValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e ListEventFeedsRequestValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e ListEventFeedsRequestValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e ListEventFeedsRequestValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e ListEventFeedsRequestValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e ListEventFeedsRequestValidationError) ErrorName() string {
+	return "ListEventFeedsRequestValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e ListEventFeedsRequestValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sListEventFeedsRequest.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = ListEventFeedsRequestValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = ListEventFeedsRequestValidationError{}
+
+// Validate checks the field values on ListEventFeedsResponse with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *ListEventFeedsResponse) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on ListEventFeedsResponse with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// ListEventFeedsResponseMultiError, or nil if none found.
+func (m *ListEventFeedsResponse) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *ListEventFeedsResponse) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	for idx, item := range m.GetList() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, ListEventFeedsResponseValidationError{
+						field:  fmt.Sprintf("List[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, ListEventFeedsResponseValidationError{
+						field:  fmt.Sprintf("List[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return ListEventFeedsResponseValidationError{
+					field:  fmt.Sprintf("List[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	if m.GetNextPageToken() != "" {
+
+		if l := len(m.GetNextPageToken()); l < 1 || l > 1048576 {
+			err := ListEventFeedsResponseValidationError{
+				field:  "NextPageToken",
+				reason: "value length must be between 1 and 1048576 bytes, inclusive",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+	}
+
+	for idx, item := range m.GetAnnotations() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, ListEventFeedsResponseValidationError{
+						field:  fmt.Sprintf("Annotations[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, ListEventFeedsResponseValidationError{
+						field:  fmt.Sprintf("Annotations[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return ListEventFeedsResponseValidationError{
+					field:  fmt.Sprintf("Annotations[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	if len(errors) > 0 {
+		return ListEventFeedsResponseMultiError(errors)
+	}
+
+	return nil
+}
+
+// ListEventFeedsResponseMultiError is an error wrapping multiple validation
+// errors returned by ListEventFeedsResponse.ValidateAll() if the designated
+// constraints aren't met.
+type ListEventFeedsResponseMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m ListEventFeedsResponseMultiError) Error() string {
+	msgs := make([]string, 0, len(m))
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m ListEventFeedsResponseMultiError) AllErrors() []error { return m }
+
+// ListEventFeedsResponseValidationError is the validation error returned by
+// ListEventFeedsResponse.Validate if the designated constraints aren't met.
+type ListEventFeedsResponseValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e ListEventFeedsResponseValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e ListEventFeedsResponseValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e ListEventFeedsResponseValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e ListEventFeedsResponseValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e ListEventFeedsResponseValidationError) ErrorName() string {
+	return "ListEventFeedsResponseValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e ListEventFeedsResponseValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sListEventFeedsResponse.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = ListEventFeedsResponseValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = ListEventFeedsResponseValidationError{}
+
 // Validate checks the field values on Event with the rules defined in the
 // proto definition for this message. If any rules are violated, the first
 // error encountered is returned, or nil if there are no violations.
