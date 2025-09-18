4e814b014cb21b20b8c0aa89f1c7ac90863fe2fc
Geoff Greer <geoff@greer.fm>
2025-09-18 16:03:37 -0700

    We need ugrpc in this file.

--- pkg/cli/lambda_server__added.go
+++ pkg/cli/lambda_server__added.go
@@ -14,6 +14,7 @@ import (
 	"github.com/conductorone/baton-sdk/pkg/crypto"
 	"github.com/conductorone/baton-sdk/pkg/crypto/providers/jwk"
 	"github.com/conductorone/baton-sdk/pkg/logging"
+	"github.com/conductorone/baton-sdk/pkg/ugrpc"
 	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
