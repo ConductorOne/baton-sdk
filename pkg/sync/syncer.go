b5103851357ff297b132db6d33f4955d69b27f20
Geoff Greer <geoff@greer.fm>
2025-12-04 15:57:30 -0800

    Move ProgressCounts into its own package. Rename it to ProgressLog, since that's what it does.

--- pkg/sync/syncer.go
+++ pkg/sync/syncer.go
@@ -12,7 +12,6 @@ import (
 	"slices"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/Masterminds/semver/v3"
@@ -39,6 +38,7 @@ import (
 	"github.com/conductorone/baton-sdk/pkg/annotations"
 	"github.com/conductorone/baton-sdk/pkg/connectorstore"
 	"github.com/conductorone/baton-sdk/pkg/dotc1z/manager"
+	"github.com/conductorone/baton-sdk/pkg/sync/progresslog"
 	"github.com/conductorone/baton-sdk/pkg/types"
 )
 
@@ -51,261 +51,6 @@ var ErrSyncNotComplete = fmt.Errorf("sync exited without finishing")
 type Syncer interface {
 	Sync(context.Context) error
 	Close(context.Context) error
-}
-
-type ProgressCounts struct {
-	ResourceTypes        int
-	Resources            map[string]int
-	EntitlementsProgress map[string]int
-	LastEntitlementLog   map[string]time.Time
-	GrantsProgress       map[string]int
-	LastGrantLog         map[string]time.Time
-	LastActionLog        time.Time
-	mu                   sync.RWMutex // Protect concurrent access to maps
-	sequentialMode       bool         // Disable mutex protection for sequential sync
-}
-
-const maxLogFrequency = 10 * time.Second
-
-// TODO: use a mutex or a syncmap for when this code becomes parallel
-func NewProgressCounts() *ProgressCounts {
-	return &ProgressCounts{
-		Resources:            make(map[string]int),
-		EntitlementsProgress: make(map[string]int),
-		LastEntitlementLog:   make(map[string]time.Time),
-		GrantsProgress:       make(map[string]int),
-		LastGrantLog:         make(map[string]time.Time),
-		LastActionLog:        time.Time{},
-		sequentialMode:       true, // Default to sequential mode for backward compatibility
-	}
-}
-
-func (p *ProgressCounts) LogResourceTypesProgress(ctx context.Context) {
-	l := ctxzap.Extract(ctx)
-	l.Info("Synced resource types", zap.Int("count", p.ResourceTypes))
-}
-
-func (p *ProgressCounts) LogResourcesProgress(ctx context.Context, resourceType string) {
-	var resources int
-	if p.sequentialMode {
-		resources = p.Resources[resourceType]
-	} else {
-		p.mu.RLock()
-		resources = p.Resources[resourceType]
-		p.mu.RUnlock()
-	}
-
-	l := ctxzap.Extract(ctx)
-	l.Info("Synced resources", zap.String("resource_type_id", resourceType), zap.Int("count", resources))
-}
-
-func (p *ProgressCounts) LogEntitlementsProgress(ctx context.Context, resourceType string) {
-	var entitlementsProgress, resources int
-	var lastLogTime time.Time
-
-	if p.sequentialMode {
-		entitlementsProgress = p.EntitlementsProgress[resourceType]
-		resources = p.Resources[resourceType]
-		lastLogTime = p.LastEntitlementLog[resourceType]
-	} else {
-		p.mu.RLock()
-		entitlementsProgress = p.EntitlementsProgress[resourceType]
-		resources = p.Resources[resourceType]
-		lastLogTime = p.LastEntitlementLog[resourceType]
-		p.mu.RUnlock()
-	}
-
-	l := ctxzap.Extract(ctx)
-	if resources == 0 {
-		// if resuming sync, resource counts will be zero, so don't calculate percentage. just log every 10 seconds.
-		if time.Since(lastLogTime) > maxLogFrequency {
-			l.Info("Syncing entitlements",
-				zap.String("resource_type_id", resourceType),
-				zap.Int("synced", entitlementsProgress),
-			)
-			if !p.sequentialMode {
-				p.mu.Lock()
-				p.LastEntitlementLog[resourceType] = time.Now()
-				p.mu.Unlock()
-			} else {
-				p.LastEntitlementLog[resourceType] = time.Now()
-			}
-		}
-		return
-	}
-
-	percentComplete := (entitlementsProgress * 100) / resources
-
-	switch {
-	case percentComplete == 100:
-		l.Info("Synced entitlements",
-			zap.String("resource_type_id", resourceType),
-			zap.Int("count", entitlementsProgress),
-			zap.Int("total", resources),
-		)
-		if !p.sequentialMode {
-			p.mu.Lock()
-			p.LastEntitlementLog[resourceType] = time.Time{}
-			p.mu.Unlock()
-		} else {
-			p.LastEntitlementLog[resourceType] = time.Time{}
-		}
-	case time.Since(lastLogTime) > maxLogFrequency:
-		if entitlementsProgress > resources {
-			l.Warn("more entitlement resources than resources",
-				zap.String("resource_type_id", resourceType),
-				zap.Int("synced", entitlementsProgress),
-				zap.Int("total", resources),
-			)
-		} else {
-			l.Info("Syncing entitlements",
-				zap.String("resource_type_id", resourceType),
-				zap.Int("synced", entitlementsProgress),
-				zap.Int("total", resources),
-				zap.Int("percent_complete", percentComplete),
-			)
-		}
-		if !p.sequentialMode {
-			p.mu.Lock()
-			p.LastEntitlementLog[resourceType] = time.Now()
-			p.mu.Unlock()
-		} else {
-			p.LastEntitlementLog[resourceType] = time.Now()
-		}
-	}
-}
-
-func (p *ProgressCounts) LogGrantsProgress(ctx context.Context, resourceType string) {
-	var grantsProgress, resources int
-	var lastLogTime time.Time
-
-	if p.sequentialMode {
-		grantsProgress = p.GrantsProgress[resourceType]
-		resources = p.Resources[resourceType]
-		lastLogTime = p.LastGrantLog[resourceType]
-	} else {
-		p.mu.RLock()
-		grantsProgress = p.GrantsProgress[resourceType]
-		resources = p.Resources[resourceType]
-		lastLogTime = p.LastGrantLog[resourceType]
-		p.mu.RUnlock()
-	}
-
-	l := ctxzap.Extract(ctx)
-	if resources == 0 {
-		// if resuming sync, resource counts will be zero, so don't calculate percentage. just log every 10 seconds.
-		if time.Since(lastLogTime) > maxLogFrequency {
-			l.Info("Syncing grants",
-				zap.String("resource_type_id", resourceType),
-				zap.Int("synced", grantsProgress),
-			)
-			if !p.sequentialMode {
-				p.mu.Lock()
-				p.LastGrantLog[resourceType] = time.Now()
-				p.mu.Unlock()
-			} else {
-				p.LastGrantLog[resourceType] = time.Now()
-			}
-		}
-		return
-	}
-
-	percentComplete := (grantsProgress * 100) / resources
-
-	switch {
-	case percentComplete == 100:
-		l.Info("Synced grants",
-			zap.String("resource_type_id", resourceType),
-			zap.Int("count", grantsProgress),
-			zap.Int("total", resources),
-		)
-		if !p.sequentialMode {
-			p.mu.Lock()
-			p.LastGrantLog[resourceType] = time.Time{}
-			p.mu.Unlock()
-		} else {
-			p.LastGrantLog[resourceType] = time.Time{}
-		}
-	case time.Since(lastLogTime) > maxLogFrequency:
-		if grantsProgress > resources {
-			l.Warn("more grant resources than resources",
-				zap.String("resource_type_id", resourceType),
-				zap.Int("synced", grantsProgress),
-				zap.Int("total", resources),
-			)
-		} else {
-			l.Info("Syncing grants",
-				zap.String("resource_type_id", resourceType),
-				zap.Int("synced", grantsProgress),
-				zap.Int("total", resources),
-				zap.Int("percent_complete", percentComplete),
-			)
-		}
-		if !p.sequentialMode {
-			p.mu.Lock()
-			p.LastGrantLog[resourceType] = time.Now()
-			p.mu.Unlock()
-		} else {
-			p.LastGrantLog[resourceType] = time.Now()
-		}
-	}
-}
-
-func (p *ProgressCounts) LogExpandProgress(ctx context.Context, actions []*expand.EntitlementGraphAction) {
-	actionsLen := len(actions)
-
-	if p.sequentialMode {
-		if time.Since(p.LastActionLog) < maxLogFrequency {
-			return
-		}
-		p.LastActionLog = time.Now()
-	} else {
-		p.mu.Lock()
-		if time.Since(p.LastActionLog) < maxLogFrequency {
-			p.mu.Unlock()
-			return
-		}
-		p.LastActionLog = time.Now()
-		p.mu.Unlock()
-	}
-
-	l := ctxzap.Extract(ctx)
-	l.Info("Expanding grants", zap.Int("actions_remaining", actionsLen))
-}
-
-// Thread-safe methods for parallel syncer
-
-// AddResourceTypes safely adds to the resource types count.
-func (p *ProgressCounts) AddResourceTypes(count int) {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	p.ResourceTypes += count
-}
-
-// AddResources safely adds to the resources count for a specific resource type.
-func (p *ProgressCounts) AddResources(resourceType string, count int) {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	p.Resources[resourceType] += count
-}
-
-// AddEntitlementsProgress safely adds to the entitlements progress count for a specific resource type.
-func (p *ProgressCounts) AddEntitlementsProgress(resourceType string, count int) {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	p.EntitlementsProgress[resourceType] += count
-}
-
-// AddGrantsProgress safely adds to the grants progress count for a specific resource type.
-func (p *ProgressCounts) AddGrantsProgress(resourceType string, count int) {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	p.GrantsProgress[resourceType] += count
-}
-
-// SetSequentialMode enables/disables mutex protection for sequential sync.
-func (p *ProgressCounts) SetSequentialMode(sequential bool) {
-	p.sequentialMode = sequential
 }
 
 // SequentialSyncer orchestrates a connector sync and stores the results using the provided datasource.Writer.
@@ -324,7 +69,7 @@ type SequentialSyncer struct {
 	tmpDir                              string
 	skipFullSync                        bool
 	lastCheckPointTime                  time.Time
-	counts                              *ProgressCounts
+	counts                              *progresslog.ProgressLog
 	targetedSyncResources               []*v2.Resource
 	onlyExpandGrants                    bool
 	dontExpandGrants                    bool
@@ -1436,13 +1181,7 @@ func (s *SequentialSyncer) syncEntitlementsForResource(ctx context.Context, reso
 			return err
 		}
 	} else {
-		if s.counts.sequentialMode {
-			s.counts.EntitlementsProgress[resourceID.ResourceType] += 1
-		} else {
-			s.counts.mu.Lock()
-			s.counts.EntitlementsProgress[resourceID.ResourceType] += 1
-			s.counts.mu.Unlock()
-		}
+		s.counts.AddEntitlementsProgress(resourceID.ResourceType, 1)
 		s.counts.LogEntitlementsProgress(ctx, resourceID.GetResourceType())
 
 		s.state.FinishAction(ctx)
@@ -3132,12 +2871,11 @@ func WithSkipGrants(skip bool) SyncOpt {
 // NewSyncer returns a new syncer object.
 func NewSyncer(ctx context.Context, c types.ConnectorClient, opts ...SyncOpt) (*SequentialSyncer, error) {
 	s := &SequentialSyncer{
-		connector:                       c,
-		skipEGForResourceType:           make(map[string]bool),
-		skipEntitlementsForResourceType: make(map[string]bool),
-		resourceTypeTraits:              make(map[string][]v2.ResourceType_Trait),
-		counts:                          NewProgressCounts(),
-		syncType:                        connectorstore.SyncTypeFull,
+		connector:             c,
+		skipEGForResourceType: make(map[string]bool),
+		resourceTypeTraits:    make(map[string][]v2.ResourceType_Trait),
+		counts:                progresslog.NewProgressCounts(),
+		syncType:              connectorstore.SyncTypeFull,
 	}
 
 	for _, o := range opts {
