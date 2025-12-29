d7ef32979f8c8a6e2d9356472bee4864a76e2242
Marcus Goldschmidt <marcus.golds@hotmail.com>
2025-12-29 15:14:42 -0400

    add option for default capabilities

---
+++ internal/tests/default_connector_capabilitites_test.go
@@ -0,0 +1,27 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
+	"github.com/conductorone/baton-sdk/pkg/field"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDefaultConnectorCapabilities(t *testing.T) {
+	ctx := t.Context()
+
+	requiredField := field.StringField("name", field.WithRequired(true))
+	carrier := field.NewConfiguration(
+		[]field.SchemaField{
+			requiredField,
+		},
+	)
+
+	t.Run("should run «capabilities» sub-command successfully", func(t *testing.T) {
+		_, err := entrypoint(ctx, carrier, []connectorrunner.Option{
+			connectorrunner.WithDefaultConnectorBuilder(&Dummy{}),
+		}, "capabilities")
+		require.NoError(t, err)
+	})
+}
