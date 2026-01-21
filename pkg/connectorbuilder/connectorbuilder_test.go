4baa910068c7741b13ad9ba6253ebfd67f0b8702
Steve Gontzes <steve.gontzes@conductorone.com>
2026-01-21 15:34:27 -0500

    fix unit test

--- pkg/connectorbuilder/connectorbuilder_test.go
+++ pkg/connectorbuilder/connectorbuilder_test.go
@@ -829,7 +829,7 @@ func TestResourceProvisioner(t *testing.T) {
 			Id: "test-entitlement",
 		}.Build(),
 	}.Build())
-	require.ErrorContains(t, err, "resource type does not have provisioner configured")
+	require.ErrorContains(t, err, "does not have provisioner configured")
 
 	// Test success case - ResourceProvisioner implemented
 	rsProvisioner := newTestResourceProvisioner("test-resource")
