501cd22a9e902855de476a0802227d7de0a9b31b
Marcus Goldschmidt <marcus.golds@hotmail.com>
2026-06-25 13:17:12 -0400

    remove new stats cmd

--- cmd/baton/main.go
+++ cmd/baton/main.go
@@ -27,7 +27,6 @@ func main() {
 	cliCmd.AddCommand(entitlementsCmd())
 	cliCmd.AddCommand(grantsCmd())
 	cliCmd.AddCommand(statsCmd())
-	cliCmd.AddCommand(syncRunInfoCmd())
 	cliCmd.AddCommand(diffCmd())
 	cliCmd.AddCommand(export())
 	cliCmd.AddCommand(principalsCmd())
--- cmd/baton/stats.go
+++ cmd/baton/stats.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"sort"
 	"strconv"
+	"time"
 
+	v2 "github.com/conductorone/baton-sdk/pb/c1/reader/v2"
 	"github.com/conductorone/baton-sdk/pkg/baton/output"
-	"github.com/conductorone/baton-sdk/pkg/connectorstore"
 	"github.com/conductorone/baton-sdk/pkg/logging"
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
@@ -44,33 +45,119 @@ func runStats(cmd *cobra.Command, args []string) error {
 	}
 	defer store.Close(ctx)
 
-	counts, err := store.SyncMeta().Stats(ctx, connectorstore.SyncTypeAny, "")
+	syncResp, err := store.GetLatestFinishedSync(ctx, &v2.SyncsReaderServiceGetLatestFinishedSyncRequest{})
 	if err != nil {
 		return err
 	}
 
+	sync := syncResp.GetSync()
+
+	if sync == nil {
+		pterm.Info.Println("No finished sync found in the c1z")
+		return nil
+	}
+
 	if outputFormat == "json" {
-		return outputManager.Output(ctx, counts)
+		return outputManager.Output(ctx, sync)
 	}
 
-	statsTable := pterm.TableData{
+	if err := renderSyncRunInfo(sync); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func renderSyncRunInfo(sync *v2.SyncRun) error {
+	startedAt := sync.GetStartedAt().AsTime()
+	endedAt := sync.GetEndedAt().AsTime()
+
+	duration := "-"
+	if sync.HasStartedAt() && sync.HasEndedAt() {
+		duration = endedAt.Sub(startedAt).Round(time.Second).String()
+	}
+
+	startedStr := "-"
+	if sync.HasStartedAt() {
+		startedStr = startedAt.Format(time.RFC3339)
+	}
+	endedStr := "-"
+	if sync.HasEndedAt() {
+		endedStr = endedAt.Format(time.RFC3339)
+	}
+
+	infoTable := pterm.TableData{
+		{"Field", "Value"},
+		{"Sync ID", sync.GetId()},
+		{"Sync Type", sync.GetSyncType()},
+		{"Parent Sync ID", sync.GetParentSyncId()},
+		{"Started At", startedStr},
+		{"Ended At", endedStr},
+		{"Duration", duration},
+		{"Sync Token", sync.GetSyncToken()},
+	}
+
+	if err := pterm.DefaultTable.WithHasHeader().WithData(infoTable).Render(); err != nil {
+		return err
+	}
+
+	stats := sync.GetStats()
+	if stats == nil {
+		pterm.Info.Println("No stats available for this sync")
+		return nil
+	}
+
+	pterm.Println()
+	totalsTable := pterm.TableData{
 		{"Type", "Count"},
+		{"Resource Types", strconv.FormatInt(stats.GetResourceTypes(), 10)},
+		{"Resources", strconv.FormatInt(stats.GetResources(), 10)},
+		{"Entitlements", strconv.FormatInt(stats.GetEntitlements(), 10)},
+		{"Grants", strconv.FormatInt(stats.GetGrants(), 10)},
+	}
+	if err := pterm.DefaultTable.WithHasHeader().WithData(totalsTable).Render(); err != nil {
+		return err
 	}
 
-	var out [][]string
-	for key, count := range counts {
-		out = append(out, []string{
-			key,
-			strconv.Itoa(int(count)),
+	resourcesByRT := stats.GetResourcesByResourceType()
+	entitlementsByRT := stats.GetEntitlementsByResourceType()
+	grantsByRT := stats.GetGrantsByResourceType()
+
+	resourceTypes := make(map[string]struct{})
+	for rt := range resourcesByRT {
+		resourceTypes[rt] = struct{}{}
+	}
+	for rt := range entitlementsByRT {
+		resourceTypes[rt] = struct{}{}
+	}
+	for rt := range grantsByRT {
+		resourceTypes[rt] = struct{}{}
+	}
+
+	if len(resourceTypes) == 0 {
+		return nil
+	}
+
+	sortedRTs := make([]string, 0, len(resourceTypes))
+	for rt := range resourceTypes {
+		sortedRTs = append(sortedRTs, rt)
+	}
+	sort.Strings(sortedRTs)
+
+	breakdownTable := pterm.TableData{
+		{"Resource Type", "Resources", "Entitlements", "Grants"},
+	}
+	for _, rt := range sortedRTs {
+		breakdownTable = append(breakdownTable, []string{
+			rt,
+			strconv.FormatInt(resourcesByRT[rt], 10),
+			strconv.FormatInt(entitlementsByRT[rt], 10),
+			strconv.FormatInt(grantsByRT[rt], 10),
 		})
 	}
 
-	sort.Slice(out, func(i int, j int) bool {
-		return out[i][0] < out[j][0]
-	})
-
-	err = pterm.DefaultTable.WithHasHeader().WithData(append(statsTable, out...)).Render()
-	if err != nil {
+	pterm.Println()
+	if err := pterm.DefaultTable.WithHasHeader().WithData(breakdownTable).Render(); err != nil {
 		return err
 	}
 
