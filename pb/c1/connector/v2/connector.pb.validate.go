d3a6e4a8a933916a386a48a0e640370ef4b87559
Connor Collins <connor.collins@conductorone.com>
2026-07-25 00:17:57 +0000

    feat: add deprecated resource type annotation
    Co-authored-by: c1-squire-dev[bot] <c1-squire-dev[bot]@users.noreply.github.com>

--- pb/c1/connector/v2/connector.pb.validate.go
+++ pb/c1/connector/v2/connector.pb.validate.go
@@ -2043,6 +2043,105 @@ var _ interface {
 	ErrorName() string
 } = OptInRequiredValidationError{}
 
+// Validate checks the field values on Deprecated with the rules defined in the
+// proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *Deprecated) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on Deprecated with the rules defined in
+// the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in DeprecatedMultiError, or
+// nil if none found.
+func (m *Deprecated) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *Deprecated) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if len(errors) > 0 {
+		return DeprecatedMultiError(errors)
+	}
+
+	return nil
+}
+
+// DeprecatedMultiError is an error wrapping multiple validation errors
+// returned by Deprecated.ValidateAll() if the designated constraints aren't met.
+type DeprecatedMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m DeprecatedMultiError) Error() string {
+	msgs := make([]string, 0, len(m))
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m DeprecatedMultiError) AllErrors() []error { return m }
+
+// DeprecatedValidationError is the validation error returned by
+// Deprecated.Validate if the designated constraints aren't met.
+type DeprecatedValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e DeprecatedValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e DeprecatedValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e DeprecatedValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e DeprecatedValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e DeprecatedValidationError) ErrorName() string { return "DeprecatedValidationError" }
+
+// Error satisfies the builtin error interface
+func (e DeprecatedValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sDeprecated.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = DeprecatedValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = DeprecatedValidationError{}
+
 // Validate checks the field values on SkipSyncAnomalyDetection with the rules
 // defined in the proto definition for this message. If any rules are
 // violated, the first error encountered is returned, or nil if there are no violations.
@@ -2257,6 +2356,8 @@ func (m *ResourceTypeCapability) validate(all bool) error {
 			}
 		}
 	}
+
+	// no validation rules for Deprecated
 
 	if len(errors) > 0 {
 		return ResourceTypeCapabilityMultiError(errors)
